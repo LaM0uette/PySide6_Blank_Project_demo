4bf129ea6688bb6cbe39b19a9e1fd2c3c8dc1aae
LaM0uette <LaM0uette.dev@gmail.com>
2021-12-26 22:21:47 +0100

    INIT

--- __main__.py
+++ __main__.py
@@ -5,10 +5,10 @@ import psutil
 
 
 if __name__ == "__main__":
-    # for proc in psutil.process_iter():
-    #     pi = proc.as_dict(attrs=['pid', 'name'])
-    #     if pi["name"] == config.nom + ".exe":
-    #         quit()
+    for proc in psutil.process_iter():
+        pi = proc.as_dict(attrs=['pid', 'name'])
+        if pi["name"] == config.nom + ".exe":
+            quit()
 
     os.startfile(os.path.abspath("src/scripts/convert_ui.bat"))
     time.sleep(0.4)
