79f5eca94e027c25301f1f4b30e5163c8f0b66ff
LaM0uette <LaM0uette.dev@gmail.com>
2021-12-26 19:49:33 +0100

    INIT

--- src/build/P_rgb.py
+++ src/build/P_rgb.py
@@ -10,3 +10,13 @@ class P_rgb(Data):
     def th4(self): return self.TH("th4")
     def bn1(self): return self.TH("bn1")
     def bn2(self): return self.TH("bn2")
+
+    def hx_th1(self): return self.TH_HEX(self.TH("th1"))
+    def hx_th2(self): return self.TH_HEX(self.TH("th2"))
+    def hx_th3(self): return self.TH_HEX(self.TH("th3"))
+    def hx_th4(self): return self.TH_HEX(self.TH("th4"))
+    def hx_bn1(self): return self.TH_HEX(self.TH("bn1"))
+    def hx_bn2(self): return self.TH_HEX(self.TH("bn2"))
+
+    def vert(self): return 78, 194, 58
+    def rouge(self): return 199, 60, 90
--- src/build/data/Data.py
+++ src/build/data/Data.py
@@ -10,10 +10,11 @@ class Data:
     def OPEN_JSON(self, fichier_json):
         with open(f"{fichier_json}.json", "r") as fichier:
             return json.load(fichier)
-
     def GET_VAL(self, val):
         js = self.OPEN_JSON(fichier_json=f"{vrb.DO_DATA}{self.grp}")
         return js.get(val)
     def TH(self, val):
         dt = self.OPEN_JSON(fichier_json=f"{vrb.DO_THEME}{config.theme}")
         return tuple(dt[val])
+    def TH_HEX(self, rgb):
+        return "#" + "%02x%02x%02x" % rgb
