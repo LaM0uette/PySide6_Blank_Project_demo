3fafb117b2760f820f85bdd1eb9d09f9a8243f79
LaM0uette <LaM0uette.dev@gmail.com>
2022-01-03 09:50:28 +0100

    INIT

--- src/In_classe.py
+++ src/In_classe.py
@@ -4,13 +4,13 @@ from .build import *
 class In_classe:
     def __init__(self, ui):
         # QLineEdit | QTextEdit | QPlainTextEdit
-        with C_le() as C_: pass
+        # with C_le() as C_: pass
 
         # QComboBox
-        with C_cb() as C_: pass
+        # with C_cb() as C_: pass
 
         # QDateEdit
-        with C_de() as C_: pass
+        # with C_de() as C_: pass
 
         # QFrame
         with C_fr() as C_:
@@ -24,29 +24,29 @@ class In_classe:
             C_.h3(ui.lb_mt_nom)
 
         # QListWidget
-        with C_lw() as C_: pass
+        # with C_lw() as C_: pass
 
         # QPushButton
         with C_pb() as C_:
             C_.quitter(ui.pb_mt_quitter)
 
         # QCheckBox
-        with C_cb() as C_: pass
+        # with C_cb() as C_: pass
 
         # QRadioButton
-        with C_rb() as C_: pass
+        # with C_rb() as C_: pass
 
         # QProgressBar
-        with C_pg() as C_: pass
+        # with C_pg() as C_: pass
 
         # QScrollBoxArea
-        with C_sca() as C_: pass
+        # with C_sca() as C_: pass
 
         # QSpinBox | QDoubleSpinBox
-        with C_sb() as C_: pass
+        # with C_sb() as C_: pass
 
         # QTableWidget
-        with C_tw() as C_: pass
+        # with C_tw() as C_: pass
 
         # QToolBox
-        with C_tb() as C_: pass
+        # with C_tb() as C_: pass
