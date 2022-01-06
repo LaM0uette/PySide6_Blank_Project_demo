feb77812f2769335156a5c017d5f73ab136d710b
LaM0uette <LaM0uette.dev@gmail.com>
2022-01-06 14:58:29 +0100

    INIT

--- src/build/classes/C_fr.py
+++ src/build/classes/C_fr.py
@@ -25,26 +25,26 @@ class C_fr(C_wg):
     def main(self, *args):
         self.STL(list(args),
                  colors = P_rgb().p_u1(),
-                 dim = P_dim().p_all(),
+                 dim = P_dim().all(),
                  bd=P_bd().all().th2(), )
 
     def menu_top(self, *args):
         self.STL(list(args),
                  colors = P_rgb().p_u1(),
-                 dim = P_dim().p_aw_mt())
+                 dim = P_dim().aw().h9())
     def body(self, *args):
         self.STL(list(args),
                  colors = P_rgb().p_u1(),
-                 dim = P_dim().p_all())
+                 dim = P_dim().all())
     def menu_bottom(self, *args):
         self.STL(list(args),
                  colors = P_rgb().p_u2(),
-                 dim = P_dim().p_aw_mb())
+                 dim = P_dim().aw().h10())
 
     def menu_bottom_dlg(self, *args):
         self.STL(list(args),
                  colors = P_rgb().p_u2(),
-                 dim = P_dim().p_aw_mb_dlg_2())
+                 dim = P_dim().aw().h9())
 
     def option_font(self, *args):
         self.STL(list(args),
@@ -60,10 +60,10 @@ class C_fr(C_wg):
     def demo(self, *args):
         self.STL(list(args),
                  colors=P_rgb().p_u1(),
-                 dim=P_dim().p_all(),
+                 dim=P_dim().all(),
                  bd=P_bd().all().bn1(), )
     def demo_tb(self, *args):
         self.STL(list(args),
                  colors=P_rgb().p_th1(),
-                 dim=P_dim().p_all(),
+                 dim=P_dim().all(),
                  bd=P_bd().bottom().bn1(),)
