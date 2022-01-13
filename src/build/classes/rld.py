8c65757ce51fe1cbde5165c4bb55fa1f4a3b049b
LaM0uette <LaM0uette.dev@gmail.com>
2022-01-13 15:13:20 +0100

    INIT

--- src/build/classes/rld.py
+++ src/build/classes/rld.py
@@ -10,6 +10,7 @@ from .Progress_bar import Progress_bar
 from .Push_button import Push_button
 from .Radio_button import Radio_button
 from .Scroll_box_area import Scroll_box_area
+from .Slider import Slider
 from .Spin_box import Spin_box
 
 
@@ -23,4 +24,5 @@ importlib.reload(Progress_bar)
 importlib.reload(Push_button)
 importlib.reload(Radio_button)
 importlib.reload(Scroll_box_area)
+importlib.reload(Slider)
 importlib.reload(Spin_box)
