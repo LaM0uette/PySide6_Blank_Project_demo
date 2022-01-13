ae3b35d4faf25a484abbad3bf927e98086b5eb4f
LaM0uette <LaM0uette.dev@gmail.com>
2022-01-13 16:56:24 +0100

    INIT

---
+++ src/build/classes/Text_edit/Text_edit.py
@@ -0,0 +1,88 @@
+from .wg import wg
+from .. import p_base
+from ....build import *
+
+
+class rtn:
+    def __init__(self, *wgs,
+                 colors_type=p_base.COLORS_TYPE,
+                 colors=p_base.COLORS,
+                 dim=p_base.DIM,
+                 align=p_base.ALIGN,
+                 word_wrap=p_base.WORD_WRAP,
+                 font=p_base.FONT,
+                 bd=p_base.BD,
+                 rd=p_base.RD
+                 ):
+        self.wgs = wgs
+
+        self.colors_type = colors_type
+        self.colors = colors
+        self.dim = dim
+        self.align = align
+        self.word_wrap = word_wrap
+        self.font = font
+        self.bd = bd
+        self.rd = rd
+
+    def rtn(self):
+        wg(
+            *self.wgs,
+            colors_type=self.colors_type,
+            colors=self.colors,
+            dim=self.dim,
+            align=self.align,
+            word_wrap=self.word_wrap,
+            font=self.font,
+            bd=self.bd,
+            rd=self.rd,
+        )
+
+    def th(self):
+        self.colors_type = "th"
+        self.rtn()
+    def tr(self):
+        self.colors_type = "tr"
+        self.rtn()
+
+    def bottom_bn1(self):
+        self.bd = P_bd().bottom().bn1()
+        self.tr()
+
+class base(rtn):
+    def __init__(self, *wgs):
+        super().__init__(*wgs,
+                         colors = P_rgb().p_th3(),
+                         font = P_font().h4(),
+                         align = P_align().l().c()
+        )
+class h1(rtn):
+    def __init__(self, *wgs):
+        super().__init__(*wgs,
+                         colors = P_rgb().p_th3(),
+                         font = P_font().h1(),
+        )
+class h2(rtn):
+    def __init__(self, *wgs):
+        super().__init__(*wgs,
+                         colors = P_rgb().p_th3(),
+                         font = P_font().h2(),
+        )
+class h3(rtn):
+    def __init__(self, *wgs):
+        super().__init__(*wgs,
+                         colors = P_rgb().p_th3(),
+                         font = P_font().h3(),
+        )
+class h4(rtn):
+    def __init__(self, *wgs):
+        super().__init__(*wgs,
+                         colors = P_rgb().p_th3(),
+                         font = P_font().h4(),
+        )
+class h5(rtn):
+    def __init__(self, *wgs):
+        super().__init__(*wgs,
+                         colors = P_rgb().p_th3(),
+                         font = P_font().h5(),
+        )
