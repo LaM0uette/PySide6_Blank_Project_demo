9efe6ebd258cd69631e0819827299de51326940d
LaM0uette <LaM0uette.dev@gmail.com>
2022-02-07 09:41:15 +0100

    REMAKE - class check box

--- src/build/widgets/Check_box/Check_box.py
+++ src/build/widgets/Check_box/Check_box.py
@@ -10,6 +10,10 @@ class Base_th:
 class Base_tr:
     def __init__(self, *wgs):
         wg(*wgs,
+           couleur_bg=(0, 0, 0, 0),
+           couleur_bg_hover=(0, 0, 0, 0),
+           couleur_bg_checked=(0, 0, 0, 0),
+           couleur_bg_checked_hover=(0, 0, 0, 0),
         )
 
 """
