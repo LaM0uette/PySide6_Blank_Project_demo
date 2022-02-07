e69b94d0fd8b25d734c1e5aaa21fcfb7ff923cdf
LaM0uette <LaM0uette.dev@gmail.com>
2022-02-07 11:31:44 +0100

    REMAKE - class combo box

--- src/build/widgets/Combo_box/Combo_box.py
+++ src/build/widgets/Combo_box/Combo_box.py
@@ -4,7 +4,9 @@ from ....build import *
 
 class Base_th:
     def __init__(self, *wgs):
-        wg(*wgs)
+        wg(*wgs,
+           curseur=P_cur().main()
+        )
 
 
 class Base_tr:
@@ -12,4 +14,5 @@ class Base_tr:
         wg(*wgs,
            couleur_bg=(0, 0, 0, 0),
            couleur_bg_hover=(0, 0, 0, 0),
+           edit=True
         )
