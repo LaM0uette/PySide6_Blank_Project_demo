5d0577fd97da4042c10c653195440e58f49bbc57
LaM0uette <LaM0uette.dev@gmail.com>
2022-02-08 22:09:43 +0100

    REMAKE - class label

--- src/build/widgets/Label/Label.py
+++ src/build/widgets/Label/Label.py
@@ -19,5 +19,13 @@ class Base_tr:
 class DemoCat:
     def __init__(self, *wgs):
         wg.wg(*wgs,
-              couleur_bg=(0, 0, 0, 0)
+              couleur_bg=(0, 0, 0, 0),
+              bordure_width_top=P_style().bd(),
+              bordure_width_bottom=P_style().bd(),
+              bordure_width_right=P_style().bd(),
+              bordure_width_left=P_style().bd(),
+              bordure_couleur_top=P_rgb().bn1(),
+              bordure_couleur_bottom=P_rgb().bn1(),
+              bordure_couleur_right=P_rgb().bn1(),
+              bordure_couleur_left=P_rgb().bn1(),
               )
