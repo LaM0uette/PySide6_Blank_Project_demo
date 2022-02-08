6cd4b2e8ff94338371e0b496caca9c06f7460407
LaM0uette <LaM0uette.dev@gmail.com>
2022-02-08 22:50:52 +0100

    REMAKE - class list widget

--- src/build/widgets/List_widget/List_widget.py
+++ src/build/widgets/List_widget/List_widget.py
@@ -13,7 +13,9 @@ class Base_tr:
     def __init__(self, *wgs):
         wg.wg(*wgs,
               couleur_bg=(0, 0, 0, 0),
+              couleur_bg_item=(0, 0, 0, 0),
               couleur_bg_checked=(0, 0, 0, 0),
+              couleur_bg_item_hover=(0, 0, 0, 0),
               couleur_bg_checked_hover=(0, 0, 0, 0),
               couleur_fg_checked=p_base.COULEURS.get("c3"),
               )
@@ -22,15 +24,19 @@ class Base_tr:
 class Demo_th:
     def __init__(self, *wgs):
         wg.wg(*wgs,
+              couleur_fg_checked=p_base.COULEURS.get("bn1"),
+              couleur_fg_checked_hover=p_base.COULEURS.get("bn2"),
               dim_height=P_dim().h5()
               )
 class Demo_tr:
     def __init__(self, *wgs):
         wg.wg(*wgs,
               couleur_bg=(0, 0, 0, 0),
+              couleur_bg_item=(0, 0, 0, 0),
               couleur_bg_checked=(0, 0, 0, 0),
               couleur_bg_item_hover=(0, 0, 0, 0),
               couleur_bg_checked_hover=(0, 0, 0, 0),
-              couleur_fg_checked=p_base.COULEURS.get("c3"),
+              couleur_fg_checked=p_base.COULEURS.get("bn1"),
+              couleur_fg_checked_hover=p_base.COULEURS.get("bn2"),
               dim_height=P_dim().h5()
               )
