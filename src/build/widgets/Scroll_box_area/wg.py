1d6e1daeb79db1cea6c575b2ea2a52c6e90605a5
LaM0uette <LaM0uette.dev@gmail.com>
2022-02-10 22:42:18 +0100

    REMAKE - class scroll box area

--- src/build/widgets/Scroll_box_area/wg.py
+++ src/build/widgets/Scroll_box_area/wg.py
@@ -7,69 +7,88 @@ from ....build import *
 class wg:
     def __init__(self,
                  *wgs,
-                 colors_type,
-                 colors,
-                 dim,
-                 bd,
-                 rd,
-                 scroll
+
+                 couleur_bg=p_base.COULEUR_BG,
+                 couleur_bg_hover=p_base.COULEUR_BG_HOVER,
+                 couleur_bg_selection=p_base.COULEUR_BG_SELECTION,
+                 couleur_bg_item=p_base.COULEUR_BG_ITEM,
+                 couleur_bg_item_hover=p_base.COULEUR_BG_ITEM_HOVER,
+                 couleur_fg=p_base.COULEUR_FG,
+                 couleur_fg_hover=p_base.COULEUR_FG_HOVER,
+                 couleur_fg_selection=p_base.COULEUR_FG_SELECTION,
+                 couleur_fg_item=p_base.COULEUR_FG_ITEM,
+                 couleur_fg_item_hover=p_base.COULEUR_FG_ITEM_HOVER,
+
+                 dim_width=p_base.DIM_WG_WIDTH,
+                 dim_height=p_base.DIM_WG_HEIGHT,
+
+                 bordure_width_top=p_base.BD_WIDTH,
+                 bordure_width_bottom=p_base.BD_WIDTH,
+                 bordure_width_right=p_base.BD_WIDTH,
+                 bordure_width_left=p_base.BD_WIDTH,
+                 bordure_style_top=p_base.BD_STYLE,
+                 bordure_style_bottom=p_base.BD_STYLE,
+                 bordure_style_right=p_base.BD_STYLE,
+                 bordure_style_left=p_base.BD_STYLE,
+                 bordure_couleur_top=p_base.BD_COULEUR,
+                 bordure_couleur_bottom=p_base.BD_COULEUR,
+                 bordure_couleur_right=p_base.BD_COULEUR,
+                 bordure_couleur_left=p_base.BD_COULEUR,
+                 rayon_top_left=p_base.RD_WG,
+                 rayon_top_right=p_base.RD_WG,
+                 rayon_bottom_right=p_base.RD_WG,
+                 rayon_bottom_left=p_base.RD_WG,
+                 scroll_bg=p_base.SCROLL_BG,
+                 scroll_handle_bg=p_base.SCROLL_HANDLE_BG,
+                 scroll_handle_fg=p_base.SCROLL_HANDLE_FG,
+                 scroll_width=p_base.SCROLL_WIDTH,
+                 scroll_height=p_base.SCROLL_HEIGHT,
+                 scroll_handle_min_width=p_base.SCROLL_HANDLE_MIN_WIDTH,
+                 scroll_handle_min_height=p_base.SCROLL_HANDLE_MIN_HEIGHT,
     ):
-        bds = Attrs(bd=bd).GET_BD()
-        rds = Attrs(rd=rd).GET_RD()
+        style = f"""
+        .QScrollArea .QWidget {{
+        background-color: rgb{colors.get("c1")};
+        }}
+
+        /* BORDURES */
+        .QScrollArea {{
+        border-top: {bordure_width_top}px {bordure_style_top} rgba{bordure_couleur_top};
+        border-bottom: {bordure_width_bottom}px {bordure_style_bottom} rgba{bordure_couleur_bottom};
+        border-right: {bordure_width_right}px {bordure_style_right} rgba{bordure_couleur_right};
+        border-left: {bordure_width_left}px {bordure_style_left} rgba{bordure_couleur_left};
+        }}
+        
+        /* RAYONS */
+        .QScrollArea {{
+        border-top-left-radius: {rayon_top_left}px;
+        border-top-right-radius: {rayon_top_right}px;
+        border-bottom-right-radius: {rayon_bottom_right}px;
+        border-bottom-left-radius: {rayon_bottom_left}px;
+        }}
+        
+        /* SCROLL */
+        QScrollArea QScrollBar {{
+        background-color: rgb{scroll_bg};
+        width: {scroll_width}px;
+        height: {scroll_height}px;
+        }}
+        QScrollArea::handle:horizontal {{
+        min-width: {scroll_handle_min_width}px;
+        }}
+        QScrollArea::handle:vertical {{
+        min-height: {scroll_handle_min_height}px;
+        }}
+        QScrollArea QScrollBar::handle {{
+        background-color: rgb{scroll_handle_fg};
+        }}
+        
+        QScrollArea QScrollBar::add-page, QScrollArea QScrollBar::sub-page {{
+        background-color: rgb{scroll_handle_bg};
+        border: none;
+        }}"""
 
         for wg in wgs:
-            style_gen = f"""
-                    /* BORDURES */
-                    .QScrollArea#{wg.objectName()} {{
-                    border-width: {bd.get("px")}px;
-                    border-style: solid;
-                    border-color: rgba{bds.get("o1")} rgba{bds.get("o2")} rgba{bds.get("o3")} rgba{bds.get("o4")};
-                    }}
-
-                    /* RAYONS */
-                    .QScrollArea#{wg.objectName()} {{
-                    border-top-left-radius: {rds.get("r1")}px;
-                    border-top-right-radius: {rds.get("r2")}px;
-                    border-bottom-right-radius: {rds.get("r4")}px;
-                    border-bottom-left-radius: {rds.get("r3")}px;
-                    }}
-
-                    /* SCROLL */
-                    .QScrollArea QScrollBar {{
-                    background-color: rgb{colors.get("c1")};
-                    width: 20px;
-                    height: 20px;
-                    }}
-                    .QScrollArea::handle:vertical {{
-                    min-height: 100px;
-                    }}
-                    .QScrollArea::handle:vertical {{
-                    min-height: 100px;
-                    }}
-                    .QScrollArea::handle:horizontal {{
-                    min-width: 100px;
-                    }}
-                    .QScrollArea QScrollBar::handle {{
-                    background-color: rgb{colors.get("c3")};
-                    }}
-                    .QScrollArea QScrollBar::add-page, .QScrollArea QScrollBar::sub-page {{
-                    background-color: rgb{colors.get("c1")};
-                    border: rgb{colors.get("c1")};
-                    }}
-            """
-            style_type = {
-                "th": f"""
-                .QScrollArea .QWidget {{
-                background-color: rgb{colors.get("c1")};
-                }}""",
-
-                "tr": f"""
-                .QScrollArea .QWidget {{
-                background-color: rgba(0, 0, 0, 0);
-                }}"""
-            }
-            style = style_gen + style_type.get(colors_type)
-
             wg.setStyleSheet(style)
 
             try:
