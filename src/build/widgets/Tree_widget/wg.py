0b06f2548df4e1022ff0efe1e43cb40ae48fdf94
LaM0uette <LaM0uette.dev@gmail.com>
2022-02-13 13:34:24 +0100

    REMAKE - class treeWidget

--- src/build/widgets/Tree_widget/wg.py
+++ src/build/widgets/Tree_widget/wg.py
@@ -1,7 +1,7 @@
 from PySide6 import QtWidgets
 
-from ..Attrs import Attrs
 from ....build import *
+from .. import p_base
 
 
 class wg:
@@ -16,109 +16,74 @@ class wg:
                  scroll,
                  cur
     ):
-        bds = Attrs(bd=bd).GET_BD()
-        rds = Attrs(rd=rd).GET_RD()
+        style = f"""
+        QHeaderView::section {{
+        background-color: rgb{colors.get("c1")};
+        color: rgb{colors.get("c3")};
+        border: none;
+        }}
+
+        QTreeWidget {{
+        background-color: rgb{colors.get("c1")};
+        }}
+
+        QTreeWidget::item {{
+        background-color: rgb{colors.get("c1")};
+        color: rgb{colors.get("c3")};
+        }}
+
+        QTreeWidget::item:hover {{
+        color: rgb{colors.get("bn1")};
+        }}
+
+        QTreeWidget::item:selected {{
+        background-color: rgb{colors.get("c3")};
+        color: rgb{colors.get("bn1")};
+        }}
+
+        QTreeWidget::item:selected:hover {{
+        color: rgb{colors.get("bn2")};
+        }}
+
+        /* BORDURES */
+        .QTreeWidget#{wg.objectName()} {{
+        border-width: {bd.get("px")}px;
+        border-style: solid;
+        border-color: rgba{bds.get("o1")} rgba{bds.get("o2")} rgba{bds.get("o3")} rgba{bds.get("o4")};
+        }}
+
+        /* RAYONS */
+        .QTreeWidget#{wg.objectName()} {{
+        border-top-left-radius: {rds.get("r1")}px;
+        border-top-right-radius: {rds.get("r2")}px;
+        border-bottom-right-radius: {rds.get("r4")}px;
+        border-bottom-left-radius: {rds.get("r3")}px;
+        }}
+
+        /* SCROLL */
+        QTreeWidget QScrollBar {{
+        background-color: rgb{colors.get("c1")};
+        width: 20px;
+        height: 20px;
+        }}
+        QTreeWidget ::handle:vertical {{
+        min-height: 100px;
+        }}
+        QTreeWidget ::handle:vertical {{
+        min-height: 100px;
+        }}
+        QTreeWidget ::handle:horizontal {{
+        min-width: 100px;
+        }}
+        QTreeWidget QScrollBar::handle {{
+        background-color: rgb{colors.get("c3")};
+        }}
+        QTreeWidget QScrollBar::add-page, QTreeWidget QScrollBar::sub-page {{
+        background-color: rgb{colors.get("c1")};
+        border: rgb{colors.get("c1")};
+        }}"""
 
         for wg in wgs:
-            style_gen = f"""
-            /* BORDURES */
-            .QTreeWidget#{wg.objectName()} {{
-            border-width: {bd.get("px")}px;
-            border-style: solid;
-            border-color: rgba{bds.get("o1")} rgba{bds.get("o2")} rgba{bds.get("o3")} rgba{bds.get("o4")};
-            }}
-
-            /* RAYONS */
-            .QTreeWidget#{wg.objectName()} {{
-            border-top-left-radius: {rds.get("r1")}px;
-            border-top-right-radius: {rds.get("r2")}px;
-            border-bottom-right-radius: {rds.get("r4")}px;
-            border-bottom-left-radius: {rds.get("r3")}px;
-            }}
-            
-            /* SCROLL */
-            QTreeWidget QScrollBar {{
-            background-color: rgb{colors.get("c1")};
-            width: 20px;
-            height: 20px;
-            }}
-            QTreeWidget ::handle:vertical {{
-            min-height: 100px;
-            }}
-            QTreeWidget ::handle:vertical {{
-            min-height: 100px;
-            }}
-            QTreeWidget ::handle:horizontal {{
-            min-width: 100px;
-            }}
-            QTreeWidget QScrollBar::handle {{
-            background-color: rgb{colors.get("c3")};
-            }}
-            QTreeWidget QScrollBar::add-page, QTreeWidget QScrollBar::sub-page {{
-            background-color: rgb{colors.get("c1")};
-            border: rgb{colors.get("c1")};
-            }}"""
-            style_type = {
-                "th": f"""
-                QHeaderView::section {{
-                background-color: rgb{colors.get("c1")};
-                color: rgb{colors.get("c3")};
-                border: none;
-                }}
-                
-                QTreeWidget {{
-                background-color: rgb{colors.get("c1")};
-                }}
-                
-                QTreeWidget::item {{
-                background-color: rgb{colors.get("c1")};
-                color: rgb{colors.get("c3")};
-                }}
-                
-                QTreeWidget::item:hover {{
-                color: rgb{colors.get("bn1")};
-                }}
-                
-                QTreeWidget::item:selected {{
-                background-color: rgb{colors.get("c3")};
-                color: rgb{colors.get("bn1")};
-                }}
-                
-                QTreeWidget::item:selected:hover {{
-                color: rgb{colors.get("bn2")};
-                }}""",
-
-                "tr": f"""
-                QHeaderView::section {{
-                background-color: rgba(0, 0, 0, 0);
-                color: rgb{colors.get("c3")};
-                border: none;
-                }}
-                
-                QTreeWidget {{
-                background-color: rgba(0, 0, 0, 0);
-                }}
-                
-                QTreeWidget::item {{
-                background-color: rgba(0, 0, 0, 0);
-                color: rgb{colors.get("c3")};
-                }}
-                
-                QTreeWidget::item:hover {{
-                background-color: rgba(0, 0, 0, 0);
-                color: rgb{colors.get("bn1")};
-                }}
-                
-                QTreeWidget::item:selected {{
-                color: rgb{colors.get("bn1")};
-                }}
-                
-                QTreeWidget::item:selected:hover {{
-                color: rgb{colors.get("bn2")};
-                }}"""
-            }
-            style = style_gen + style_type.get(colors_type)
-
             wg.setStyleSheet(style)
 
             try:
