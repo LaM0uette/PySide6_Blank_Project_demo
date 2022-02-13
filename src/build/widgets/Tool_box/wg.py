161a801bed44c533b89da207200c08fb10e70954
LaM0uette <LaM0uette.dev@gmail.com>
2022-02-13 13:29:57 +0100

    REMAKE - class toolBox

--- src/build/widgets/Tool_box/wg.py
+++ src/build/widgets/Tool_box/wg.py
@@ -1,111 +1,120 @@
 from PySide6 import QtWidgets
 
-from ..Attrs import Attrs
 from ....build import *
+from .. import p_base
 
 
 class wg:
     def __init__(self,
                  *wgs,
-                 colors_type,
-                 colors,
-                 dim,
-                 font,
-                 bd,
-                 rd,
-                 cur
+
+                 couleur_bg=p_base.COULEUR_BG,
+                 couleur_bg_hover=p_base.COULEUR_BG_HOVER,
+                 couleur_bg_checked=p_base.COULEUR_BG_CHECKED,
+                 couleur_bg_checked_hover=p_base.COULEUR_BG_CHECKED_HOVER,
+                 couleur_fg=p_base.COULEUR_FG,
+                 couleur_fg_hover=p_base.COULEUR_FG_HOVER,
+                 couleur_fg_checked=p_base.COULEUR_FG_CHECKED,
+                 couleur_fg_checked_hover=p_base.COULEUR_FG_CHECKED_HOVER,
+
+                 dim_width=p_base.DIM_WG_WIDTH,
+                 dim_height=p_base.DIM_WG_HEIGHT,
+
+                 police=p_base.FONT,
+                 police_taille=p_base.FONT_SIZE,
+
+                 bordure_width_top=p_base.BD_WIDTH,
+                 bordure_width_bottom=p_base.BD_WIDTH,
+                 bordure_width_right=p_base.BD_WIDTH,
+                 bordure_width_left=p_base.BD_WIDTH,
+                 bordure_style_top=p_base.BD_STYLE,
+                 bordure_style_bottom=p_base.BD_STYLE,
+                 bordure_style_right=p_base.BD_STYLE,
+                 bordure_style_left=p_base.BD_STYLE,
+                 bordure_couleur_top=p_base.BD_COULEUR,
+                 bordure_couleur_bottom=p_base.BD_COULEUR,
+                 bordure_couleur_right=p_base.BD_COULEUR,
+                 bordure_couleur_left=p_base.BD_COULEUR,
+                 rayon_top_left=p_base.RD_WG,
+                 rayon_top_right=p_base.RD_WG,
+                 rayon_bottom_right=p_base.RD_WG,
+                 rayon_bottom_left=p_base.RD_WG,
+                 scroll_bg=p_base.SCROLL_BG,
+                 scroll_handle_bg=p_base.SCROLL_HANDLE_BG,
+                 scroll_handle_fg=p_base.SCROLL_HANDLE_FG,
+                 scroll_width=p_base.SCROLL_WIDTH,
+                 scroll_height=p_base.SCROLL_HEIGHT,
+                 scroll_handle_min_width=p_base.SCROLL_HANDLE_MIN_WIDTH,
+                 scroll_handle_min_height=p_base.SCROLL_HANDLE_MIN_HEIGHT,
+
+                 curseur=p_base.CUR
     ):
-        bds = Attrs(bd=bd).GET_BD()
-        rds = Attrs(rd=rd).GET_RD()
+        style = f"""
+        QToolBox::tab {{
+        background-color: rgb{couleur_bg};
+        color: rgb{couleur_fg};
+        }}
+
+        QToolBox::tab:hover {{
+        background-color: rgb{couleur_bg_hover};
+        color: rgb{couleur_fg_hover};
+        }}
+
+        QToolBox::tab:selected {{
+        background-color: rgb{couleur_bg_checked};
+        color: rgb{couleur_fg_checked};
+        }}
+
+        QToolBox::tab:selected:hover {{
+        background-color: rgb{couleur_bg_checked_hover};
+        color: rgb{couleur_fg_checked_hover};
+        }}
+
+        /* BORDURES */
+        .QToolBox {{
+        border-top: {bordure_width_top}px {bordure_style_top} rgba{bordure_couleur_top};
+        border-bottom: {bordure_width_bottom}px {bordure_style_bottom} rgba{bordure_couleur_bottom};
+        border-right: {bordure_width_right}px {bordure_style_right} rgba{bordure_couleur_right};
+        border-left: {bordure_width_left}px {bordure_style_left} rgba{bordure_couleur_left};
+        }}
+        
+        /* RAYONS */
+        .QToolBox {{
+        border-top-left-radius: {rayon_top_left}px;
+        border-top-right-radius: {rayon_top_right}px;
+        border-bottom-right-radius: {rayon_bottom_right}px;
+        border-bottom-left-radius: {rayon_bottom_left}px;
+        }}
+
+        /* SCROLL */
+        .QToolBox QScrollBar {{
+        background-color: rgb{scroll_bg};
+        width: {scroll_width}px;
+        height: {scroll_height}px;
+        }}
+        .QToolBox::handle:horizontal {{
+        min-width: {scroll_handle_min_width}px;
+        }}
+        .QToolBox::handle:vertical {{
+        min-height: {scroll_handle_min_height}px;
+        }}
+        .QToolBox QScrollBar::handle {{
+        background-color: rgb{scroll_handle_fg};
+        }}
+        
+        .QToolBox QScrollBar::add-page, .QToolBox QScrollBar::sub-page {{
+        background-color: rgb{scroll_handle_bg};
+        border: none;
+        }}"""
 
         for wg in wgs:
-            style_gen = f"""
-            /* BORDURES */
-            .QToolBox#{wg.objectName()} {{
-            border-width: {bd.get("px")}px;
-            border-style: solid;
-            border-color: rgba{bds.get("o1")} rgba{bds.get("o2")} rgba{bds.get("o3")} rgba{bds.get("o4")};
-            }}
-
-            /* RAYONS */
-            .QToolBox#{wg.objectName()} {{
-            border-top-left-radius: {rds.get("r1")}px;
-            border-top-right-radius: {rds.get("r2")}px;
-            border-bottom-right-radius: {rds.get("r4")}px;
-            border-bottom-left-radius: {rds.get("r3")}px;
-            }}
-            
-            /* SCROLL */
-            QToolBox QScrollBar {{
-            background-color: rgb{colors.get("c1")};
-            width: 20px;
-            height: 20px;
-            }}
-            QToolBox ::handle:vertical {{
-            min-height: 100px;
-            }}
-            QToolBox ::handle:vertical {{
-            min-height: 100px;
-            }}
-            QToolBox ::handle:horizontal {{
-            min-width: 100px;
-            }}
-            QToolBox QScrollBar::handle {{
-            background-color: rgb{colors.get("c3")};
-            }}
-            QToolBox QScrollBar::add-page, QToolBox QScrollBar::sub-page {{
-            background-color: rgb{colors.get("c1")};
-            border: rgb{colors.get("c1")};
-            }}"""
-            style_type = {
-                "th": f"""
-                QToolBox::tab {{
-                background-color: rgb{colors.get("c1")};
-                color: rgb{colors.get("c3")};
-                }}
-                
-                QToolBox::tab:hover {{
-                color: rgb{colors.get("bn1")};
-                }}
-                
-                QToolBox::tab:selected {{
-                background-color: rgb{colors.get("c1")};
-                color: rgb{colors.get("bn1")};
-                }}
-                
-                QToolBox::tab:selected:hover {{
-                color: rgb{colors.get("bn2")};
-                }}
-                """,
-
-                "tr": f"""
-                QToolBox::tab {{
-                background-color: rgba(0, 0, 0, 0);
-                color: rgb{colors.get("c1")};
-                }}
-                
-                QToolBox::tab:hover {{
-                color: rgb{colors.get("bn1")};
-                }}
-                
-                QToolBox::tab:selected {{
-                color: rgb{colors.get("bn1")};
-                }}
-                
-                QToolBox::tab:selected:hover {{
-                color: rgb{colors.get("bn2")};
-                }}
-                """
-            }
-            style = style_gen + style_type.get(colors_type)
-
             wg.setStyleSheet(style)
 
             try:
-                Fct(wg=wg, w=dim.get("w"), h=dim.get("h")).DIM()
-                wg.setFont(Fct(font_size=font).FONT())
+                Fct(wg=wg, w=dim_width, h=dim_height).DIM()
+                wg.setFont(Fct(font=police, font_size=police_taille).FONT())
 
                 wg.setFrameShape(QtWidgets.QFrame.NoFrame)
 
-                wg.setCursor(Fct(cur=cur).CUR())
+                wg.setCursor(Fct(cur=curseur).CUR())
             except: pass
