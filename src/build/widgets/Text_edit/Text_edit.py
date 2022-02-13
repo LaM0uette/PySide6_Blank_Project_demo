4d648867f645a6be5ddfd842d5f6d426dac11468
LaM0uette <LaM0uette.dev@gmail.com>
2022-02-13 12:47:58 +0100

    REMAKE - class scroll text edit

--- src/build/widgets/Text_edit/Text_edit.py
+++ src/build/widgets/Text_edit/Text_edit.py
@@ -13,22 +13,17 @@ class Base_tr:
     def __init__(self, *wgs):
         wg.wg(*wgs,
               couleur_bg=(0, 0, 0, 0),
-              couleur_bg_hover=(0, 0, 0, 0),
-              couleur_bg_checked=(0, 0, 0, 0),
-              couleur_bg_checked_hover=(0, 0, 0, 0),
-              couleur_fg_checked=p_base.COULEURS.get("c3"),
               )
 
 
 class Demo_th:
     def __init__(self, *wgs):
-        wg.wg(*wgs)
+        wg.wg(*wgs,
+              dim_height=P_dim().h5()
+              )
 class Demo_tr:
     def __init__(self, *wgs):
         wg.wg(*wgs,
               couleur_bg=(0, 0, 0, 0),
-              couleur_bg_hover=(0, 0, 0, 0),
-              couleur_bg_checked=(0, 0, 0, 0),
-              couleur_bg_checked_hover=(0, 0, 0, 0),
-              couleur_fg_checked=p_base.COULEURS.get("c3"),
+              dim_height=P_dim().h5()
               )
