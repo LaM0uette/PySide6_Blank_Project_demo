745203ecbba6b405133d709295c83033ef191d1d
LaM0uette <LaM0uette.dev@gmail.com>
2022-02-13 13:37:02 +0100

    REMAKE - class treeWidget

--- src/build/widgets/Tree_widget/wg.py
+++ src/build/widgets/Tree_widget/wg.py
@@ -7,14 +7,51 @@ from .. import p_base
 class wg:
     def __init__(self,
                  *wgs,
-                 colors_type,
-                 colors,
-                 dim,
-                 font,
-                 bd,
-                 rd,
-                 scroll,
-                 cur
+
+                 couleur_bg=p_base.COULEUR_BG,
+                 couleur_bg_hover=p_base.COULEUR_BG_HOVER,
+                 couleur_bg_selection=p_base.COULEUR_BG_SELECTION,
+                 couleur_bg_item=p_base.COULEUR_BG_ITEM,
+                 couleur_bg_item_hover=p_base.COULEUR_BG_ITEM_HOVER,
+                 couleur_fg=p_base.COULEUR_FG,
+                 couleur_fg_hover=p_base.COULEUR_FG_HOVER,
+                 couleur_fg_selection=p_base.COULEUR_FG_SELECTION,
+                 couleur_fg_item=p_base.COULEUR_FG_ITEM,
+                 couleur_fg_item_hover=p_base.COULEUR_FG_ITEM_HOVER,
+
+                 dim_width=p_base.DIM_WIDTH,
+                 dim_height=p_base.DIM_HEIGHT,
+
+                 police=p_base.FONT,
+                 police_taille=p_base.FONT_SIZE,
+
+                 bordure_width_top=p_base.BD_WIDTH,
+                 bordure_width_bottom=p_base.BD_WIDTH,
+                 bordure_width_right=p_base.BD_WIDTH,
+                 bordure_width_left=p_base.BD_WIDTH,
+                 bordure_style_top=p_base.BD_STYLE,
+                 bordure_style_bottom=p_base.BD_STYLE,
+                 bordure_style_right=p_base.BD_STYLE,
+                 bordure_style_left=p_base.BD_STYLE,
+                 bordure_couleur_top=p_base.BD_COULEUR,
+                 bordure_couleur_bottom=p_base.BD_COULEUR,
+                 bordure_couleur_right=p_base.BD_COULEUR,
+                 bordure_couleur_left=p_base.BD_COULEUR,
+                 rayon_top_left=p_base.RD_WG,
+                 rayon_top_right=p_base.RD_WG,
+                 rayon_bottom_right=p_base.RD_WG,
+                 rayon_bottom_left=p_base.RD_WG,
+                 scroll_bg=p_base.SCROLL_BG,
+                 scroll_handle_bg=p_base.SCROLL_HANDLE_BG,
+                 scroll_handle_fg=p_base.SCROLL_HANDLE_FG,
+                 scroll_width=p_base.SCROLL_WIDTH,
+                 scroll_height=p_base.SCROLL_HEIGHT,
+                 scroll_handle_min_width=p_base.SCROLL_HANDLE_MIN_WIDTH,
+                 scroll_handle_min_height=p_base.SCROLL_HANDLE_MIN_HEIGHT,
+                 scroll_h=p_base.SCROLL_H,
+                 scroll_v=p_base.SCROLL_V,
+
+                 curseur=p_base.CUR
     ):
         style = f"""
         QHeaderView::section {{
@@ -46,54 +83,53 @@ class wg:
         }}
 
         /* BORDURES */
-        .QTreeWidget#{wg.objectName()} {{
-        border-width: {bd.get("px")}px;
-        border-style: solid;
-        border-color: rgba{bds.get("o1")} rgba{bds.get("o2")} rgba{bds.get("o3")} rgba{bds.get("o4")};
+        .QTreeWidget {{
+        border-top: {bordure_width_top}px {bordure_style_top} rgba{bordure_couleur_top};
+        border-bottom: {bordure_width_bottom}px {bordure_style_bottom} rgba{bordure_couleur_bottom};
+        border-right: {bordure_width_right}px {bordure_style_right} rgba{bordure_couleur_right};
+        border-left: {bordure_width_left}px {bordure_style_left} rgba{bordure_couleur_left};
         }}
-
+        
         /* RAYONS */
-        .QTreeWidget#{wg.objectName()} {{
-        border-top-left-radius: {rds.get("r1")}px;
-        border-top-right-radius: {rds.get("r2")}px;
-        border-bottom-right-radius: {rds.get("r4")}px;
-        border-bottom-left-radius: {rds.get("r3")}px;
+        .QTreeWidget {{
+        border-top-left-radius: {rayon_top_left}px;
+        border-top-right-radius: {rayon_top_right}px;
+        border-bottom-right-radius: {rayon_bottom_right}px;
+        border-bottom-left-radius: {rayon_bottom_left}px;
         }}
 
         /* SCROLL */
-        QTreeWidget QScrollBar {{
-        background-color: rgb{colors.get("c1")};
-        width: 20px;
-        height: 20px;
+        .QTreeWidget QScrollBar {{
+        background-color: rgb{scroll_bg};
+        width: {scroll_width}px;
+        height: {scroll_height}px;
         }}
-        QTreeWidget ::handle:vertical {{
-        min-height: 100px;
+        .QTreeWidget::handle:horizontal {{
+        min-width: {scroll_handle_min_width}px;
         }}
-        QTreeWidget ::handle:vertical {{
-        min-height: 100px;
+        .QTreeWidget::handle:vertical {{
+        min-height: {scroll_handle_min_height}px;
         }}
-        QTreeWidget ::handle:horizontal {{
-        min-width: 100px;
+        .QTreeWidget QScrollBar::handle {{
+        background-color: rgb{scroll_handle_fg};
         }}
-        QTreeWidget QScrollBar::handle {{
-        background-color: rgb{colors.get("c3")};
-        }}
-        QTreeWidget QScrollBar::add-page, QTreeWidget QScrollBar::sub-page {{
-        background-color: rgb{colors.get("c1")};
-        border: rgb{colors.get("c1")};
+        
+        .QTreeWidget QScrollBar::add-page, .QTreeWidget QScrollBar::sub-page {{
+        background-color: rgb{scroll_handle_bg};
+        border: none;
         }}"""
 
         for wg in wgs:
             wg.setStyleSheet(style)
 
             try:
-                Fct(wg=wg, w=dim.get("w"), h=dim.get("h")).DIM()
-                wg.setFont(Fct(font_size=font).FONT())
+                Fct(wg=wg, w=dim_width, h=dim_height).DIM()
+                wg.setFont(Fct(font=police, font_size=police_taille).FONT())
 
-                wg.setHorizontalScrollBarPolicy(scroll.get("h"))
-                wg.setVerticalScrollBarPolicy(scroll.get("v"))
+                wg.setHorizontalScrollBarPolicy(scroll_h)
+                wg.setVerticalScrollBarPolicy(scroll_v)
 
                 wg.setFrameShape(QtWidgets.QFrame.NoFrame)
 
-                wg.setCursor(Fct(cur=cur).CUR())
+                wg.setCursor(Fct(cur=curseur).CUR())
             except: pass
