599ed1e9364947bcd7dea557ec48ea754344415d
LaM0uette <lam0uette.dev@gmail.com>
2022-02-14 14:12:12 +0100

    REMAKE - UPGRADE - CheckBox

---
+++ src/build/palettes/Rgb.py
@@ -0,0 +1,20 @@
+from .data.Data import Data
+
+class Rgb(Data):
+    def __init__(self):
+        super().__init__()
+
+    def th1(self): return self.TH("th1")+(255, )
+    def th2(self): return self.TH("th2")+(255, )
+    def th3(self): return self.TH("th3")+(255, )
+    def bn1(self): return self.TH("bn1")+(255, )
+    def bn2(self): return self.TH("bn2")+(255, )
+
+    def hx_th1(self): return self.TH_HEX(self.TH("th1"))
+    def hx_th2(self): return self.TH_HEX(self.TH("th2"))
+    def hx_th3(self): return self.TH_HEX(self.TH("th3"))
+    def hx_bn1(self): return self.TH_HEX(self.TH("bn1"))
+    def hx_bn2(self): return self.TH_HEX(self.TH("bn2"))
+
+    def vert(self): return 78, 194, 58, 255
+    def rouge(self): return 199, 60, 90, 255
