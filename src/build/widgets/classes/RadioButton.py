9cb41462245950e73430225e8691b8318fbee1d4
LaM0uette <lam0uette.dev@gmail.com>
2022-02-16 08:56:38 +0100

    REMAKE - UPGRADE - RadioButton

--- src/build/widgets/classes/RadioButton.py
+++ src/build/widgets/classes/RadioButton.py
@@ -46,24 +46,134 @@ class Style:
                  img_margin_right=0,
                  img_margin_left=(p_base.WG_HEIGHT - (p_base.WG_HEIGHT * P_style().x_ico())) / 2,
 
-                 bordure_width_top=p_base.WG_BORDER_WIDTH,
-                 bordure_width_bottom=p_base.WG_BORDER_WIDTH,
-                 bordure_width_right=p_base.WG_BORDER_WIDTH,
-                 bordure_width_left=p_base.WG_BORDER_WIDTH,
-                 bordure_style_top=p_base.WG_BORDER_STYLE,
-                 bordure_style_bottom=p_base.WG_BORDER_STYLE,
-                 bordure_style_right=p_base.WG_BORDER_STYLE,
-                 bordure_style_left=p_base.WG_BORDER_STYLE,
-                 bordure_couleur_top=p_base.WG_BORDER_RGB,
-                 bordure_couleur_bottom=p_base.WG_BORDER_RGB,
-                 bordure_couleur_right=p_base.WG_BORDER_RGB,
-                 bordure_couleur_left=p_base.WG_BORDER_RGB,
-                 rayon_top_left=p_base.WG_RADIUS,
-                 rayon_top_right=p_base.WG_RADIUS,
-                 rayon_bottom_right=p_base.WG_RADIUS,
-                 rayon_bottom_left=p_base.WG_RADIUS,
-                 curseur=p_base.CUR
-                 ):
+                 # Bordures GEN
+                 border_gen_all=None,
+                 border_gen_style=None,
+                 border_gen_rgb=None,
+                 border_gen_top=None, border_gen_bottom=None, border_gen_right=None, border_gen_left=None,
+                 # Bordures
+                 border_all=None,
+                 border_style=p_base.WG_BORDER_STYLE,
+                 border_rgb=p_base.WG_BORDER_RGB,
+                 border_top=p_base.WG_BORDER_WIDTH, border_bottom=p_base.WG_BORDER_WIDTH, border_right=p_base.WG_BORDER_WIDTH, border_left=p_base.WG_BORDER_WIDTH,
+                 # Bordures hover
+                 border_all_hover=None,
+                 border_style_hover=p_base.WG_BORDER_STYLE,
+                 border_rgb_hover=p_base.WG_BORDER_RGB,
+                 border_top_hover=p_base.WG_BORDER_WIDTH, border_bottom_hover=p_base.WG_BORDER_WIDTH, border_right_hover=p_base.WG_BORDER_WIDTH, border_left_hover=p_base.WG_BORDER_WIDTH,
+                 # Bordures checked
+                 border_all_checked=None,
+                 border_style_checked=p_base.WG_BORDER_STYLE,
+                 border_rgb_checked=p_base.WG_BORDER_RGB,
+                 border_top_checked=p_base.WG_BORDER_WIDTH, border_bottom_checked=p_base.WG_BORDER_WIDTH, border_right_checked=p_base.WG_BORDER_WIDTH, border_left_checked=p_base.WG_BORDER_WIDTH,
+                 # Bordures checked hover
+                 border_all_checked_hover=None,
+                 border_style_checked_hover=p_base.WG_BORDER_STYLE,
+                 border_rgb_checked_hover=p_base.WG_BORDER_RGB,
+                 border_top_checked_hover=p_base.WG_BORDER_WIDTH, border_bottom_checked_hover=p_base.WG_BORDER_WIDTH, border_right_checked_hover=p_base.WG_BORDER_WIDTH, border_left_checked_hover=p_base.WG_BORDER_WIDTH,
+
+                 # Rayons
+                 radius_all=None,
+                 radius_top_right=p_base.WG_RADIUS,
+                 radius_top_left=p_base.WG_RADIUS,
+                 radius_bottom_right=p_base.WG_RADIUS,
+                 radius_bottom_left=p_base.WG_RADIUS,
+
+                 # Curseur
+                 curseur=P_cur().souris_main()
+    ):
+        # BG
+        if not bg_gen is None:
+            bg = bg_gen
+            bg_hover = bg_gen
+            bg_checked = bg_gen
+            bg_checked_hover = bg_gen
+        # FG
+        if not fg_gen is None:
+            fg = fg_gen
+            fg_hover = fg_gen
+            fg_checked = fg_gen
+            fg_checked_hover = fg_gen
+        # Bordure
+        if not border_gen_all is None:
+            border_top = border_gen_all
+            border_bottom = border_gen_all
+            border_right = border_gen_all
+            border_left = border_gen_all
+            border_top_hover = border_gen_all
+            border_bottom_hover = border_gen_all
+            border_right_hover = border_gen_all
+            border_left_hover = border_gen_all
+            border_top_checked = border_gen_all
+            border_bottom_checked = border_gen_all
+            border_right_checked = border_gen_all
+            border_left_checked = border_gen_all
+            border_top_checked_hover = border_gen_all
+            border_bottom_checked_hover = border_gen_all
+            border_right_checked_hover = border_gen_all
+            border_left_checked_hover = border_gen_all
+        elif border_gen_all is None:
+            if not border_all is None:
+                border_top = border_all
+                border_bottom = border_all
+                border_right = border_all
+                border_left = border_all
+            if not border_all_hover is None:
+                border_top_hover = border_all_hover
+                border_bottom_hover = border_all_hover
+                border_right_hover = border_all_hover
+                border_left_hover = border_all_hover
+            if not border_all_checked is None:
+                border_top_checked = border_all_checked
+                border_bottom_checked = border_all_checked
+                border_right_checked = border_all_checked
+                border_left_checked = border_all_checked
+            if not border_all_checked_hover is None:
+                border_top_checked_hover = border_all_checked_hover
+                border_bottom_checked_hover = border_all_checked_hover
+                border_right_checked_hover = border_all_checked_hover
+                border_left_checked_hover = border_all_checked_hover
+
+            if not border_gen_top is None:
+                border_top = border_gen_top
+                border_top_hover = border_gen_top
+                border_top_checked = border_gen_top
+                border_top_checked_hover = border_gen_top
+            if not border_gen_bottom is None:
+                border_bottom = border_gen_bottom
+                border_bottom_hover = border_gen_bottom
+                border_bottom_checked = border_gen_bottom
+                border_bottom_checked_hover = border_gen_bottom
+            if not border_gen_right is None:
+                border_right = border_gen_right
+                border_right_hover = border_gen_right
+                border_right_checked = border_gen_right
+                border_right_checked_hover = border_gen_right
+            if not border_gen_left is None:
+                border_left = border_gen_left
+                border_left_hover = border_gen_left
+                border_left_checked = border_gen_left
+                border_left_checked_hover = border_gen_left
+        # Bordure style
+        if not border_gen_style is None:
+            border_style = border_gen_style
+            border_style_hover = border_gen_style
+            border_style_checked = border_gen_style
+            border_style_checked_hover = border_gen_style
+        # Bordure RGB
+        if not border_gen_rgb is None:
+            border_rgb = border_gen_rgb
+            border_rgb_hover = border_gen_rgb
+            border_rgb_checked = border_gen_rgb
+            border_rgb_checked_hover = border_gen_rgb
+        # Radius
+        if not radius_all is None:
+            radius_top_right = radius_all
+            radius_top_left = radius_all
+            radius_bottom_right = radius_all
+            radius_bottom_left = radius_all
+
+
         style = f"""
         /* RADIOBUTTON */
         QRadioButton {{
@@ -108,18 +218,36 @@ class Style:
 
         /* BORDURES */
         .QRadioButton {{
-        border-top: {bordure_width_top}px {bordure_style_top} rgba{bordure_couleur_top};
-        border-bottom: {bordure_width_bottom}px {bordure_style_bottom} rgba{bordure_couleur_bottom};
-        border-right: {bordure_width_right}px {bordure_style_right} rgba{bordure_couleur_right};
-        border-left: {bordure_width_left}px {bordure_style_left} rgba{bordure_couleur_left};
+        border-top: {border_top}px {border_style} rgba{border_rgb};
+        border-bottom: {border_bottom}px {border_style} rgba{border_rgb};
+        border-right: {border_right}px {border_style} rgba{border_rgb};
+        border-left: {border_left}px {border_style} rgba{border_rgb};
+        }}
+        .QRadioButton:hover {{
+        border-top: {border_top_hover}px {border_style_hover} rgba{border_rgb_hover};
+        border-bottom: {border_bottom_hover}px {border_style_hover} rgba{border_rgb_hover};
+        border-right: {border_right_hover}px {border_style_hover} rgba{border_rgb_hover};
+        border-left: {border_left_hover}px {border_style_hover} rgba{border_rgb_hover};
+        }}
+        .QRadioButton:checked {{
+        border-top: {border_top_checked}px {border_style_checked} rgba{border_rgb_checked};
+        border-bottom: {border_bottom_checked}px {border_style_checked} rgba{border_rgb_checked};
+        border-right: {border_right_checked}px {border_style_checked} rgba{border_rgb_checked};
+        border-left: {border_left_checked}px {border_style_checked} rgba{border_rgb_checked};
+        }}
+        .QRadioButton:checked:hover {{
+        border-top: {border_top_checked_hover}px {border_style_checked_hover} rgba{border_rgb_checked_hover};
+        border-bottom: {border_bottom_checked_hover}px {border_style_checked_hover} rgba{border_rgb_checked_hover};
+        border-right: {border_right_checked_hover}px {border_style_checked_hover} rgba{border_rgb_checked_hover};
+        border-left: {border_left_checked_hover}px {border_style_checked_hover} rgba{border_rgb_checked_hover};
         }}
 
         /* RAYONS */
         .QRadioButton {{
-        border-top-left-radius: {rayon_top_left}px;
-        border-top-right-radius: {rayon_top_right}px;
-        border-bottom-right-radius: {rayon_bottom_right}px;
-        border-bottom-left-radius: {rayon_bottom_left}px;
+        border-top-left-radius: {radius_top_left}px;
+        border-top-right-radius: {radius_top_right}px;
+        border-bottom-right-radius: {radius_bottom_right}px;
+        border-bottom-left-radius: {radius_bottom_left}px;
         }}"""
 
         for wg in wgs:
