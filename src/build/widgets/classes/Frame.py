376fc8739818a0d3496237f7bc6479b83e62baa2
LaM0uette <lam0uette.dev@gmail.com>
2022-02-16 14:53:02 +0100

    REMAKE - UPGRADE - Frame

--- src/build/widgets/classes/Frame.py
+++ src/build/widgets/classes/Frame.py
@@ -9,52 +9,67 @@ class Style:
 
             # Widgets
             *wgs,
-            couleur_bg=p_base._COLORS_BG,
-            dim_width=p_base.WIDTH,
-            dim_height=p_base.HEIGHT,
-            bordure_width_top=p_base.WG_BORDER_WIDTH,
-            bordure_width_bottom=p_base.WG_BORDER_WIDTH,
-            bordure_width_right=p_base.WG_BORDER_WIDTH,
-            bordure_width_left=p_base.WG_BORDER_WIDTH,
-            bordure_style_top=p_base.WG_BORDER_STYLE,
-            bordure_style_bottom=p_base.WG_BORDER_STYLE,
-            bordure_style_right=p_base.WG_BORDER_STYLE,
-            bordure_style_left=p_base.WG_BORDER_STYLE,
-            bordure_couleur_top=p_base.WG_BORDER_RGB,
-            bordure_couleur_bottom=p_base.WG_BORDER_RGB,
-            bordure_couleur_right=p_base.WG_BORDER_RGB,
-            bordure_couleur_left=p_base.WG_BORDER_RGB,
-            rayon_top_left=p_base.WG_RADIUS,
-            rayon_top_right=p_base.WG_RADIUS,
-            rayon_bottom_right=p_base.WG_RADIUS,
-            rayon_bottom_left=p_base.WG_RADIUS,
+
+            # Couleurs BG
+            bg=p_base.BG,
+
+            # Dimensions WG
+            width=p_base.WIDTH,
+            height=p_base.HEIGHT,
+
+            # Bordures
+            border_all=None,
+            border_style=p_base.WG_BORDER_STYLE,
+            border_rgb=p_base.WG_BORDER_RGB,
+            border_top=p_base.WG_BORDER_WIDTH, border_bottom=p_base.WG_BORDER_WIDTH, border_right=p_base.WG_BORDER_WIDTH, border_left=p_base.WG_BORDER_WIDTH,
+
+            # Rayons
+            radius_all=None,
+            radius_top_right=p_base.WG_RADIUS,
+            radius_top_left=p_base.WG_RADIUS,
+            radius_bottom_right=p_base.WG_RADIUS,
+            radius_bottom_left=p_base.WG_RADIUS,
     ):
+        # Bordure
+        if not border_all is None:
+            border_top = border_all
+            border_bottom = border_all
+            border_right = border_all
+            border_left = border_all
+        # Radius
+        if not radius_all is None:
+            radius_top_right = radius_all
+            radius_top_left = radius_all
+            radius_bottom_right = radius_all
+            radius_bottom_left = radius_all
+
+
         style = f"""
         /* FRAME */
         .QFrame {{
-        background-color: rgba{couleur_bg};
+        background-color: rgba{bg};
         }}
 
         /* BORDURES */
         .QFrame {{
-        border-top: {bordure_width_top}px {bordure_style_top} rgba{bordure_couleur_top};
-        border-bottom: {bordure_width_bottom}px {bordure_style_bottom} rgba{bordure_couleur_bottom};
-        border-right: {bordure_width_right}px {bordure_style_right} rgba{bordure_couleur_right};
-        border-left: {bordure_width_left}px {bordure_style_left} rgba{bordure_couleur_left};
+        border-top: {border_top}px {border_style} rgba{border_rgb};
+        border-bottom: {border_bottom}px {border_style} rgba{border_rgb};
+        border-right: {border_right}px {border_style} rgba{border_rgb};
+        border-left: {border_left}px {border_style} rgba{border_rgb};
         }}
 
         /* RAYONS */
         .QFrame {{
-        border-top-left-radius: {rayon_top_left}px;
-        border-top-right-radius: {rayon_top_right}px;
-        border-bottom-right-radius: {rayon_bottom_right}px;
-        border-bottom-left-radius: {rayon_bottom_left}px;
+        border-top-right-radius: {radius_top_right}px;
+        border-top-left-radius: {radius_top_left}px;
+        border-bottom-right-radius: {radius_bottom_right}px;
+        border-bottom-left-radius: {radius_bottom_left}px;
         }}"""
 
         for wg in wgs:
             wg.setStyleSheet(style)
 
-            Fct(wg=wg, w=dim_width, h=dim_height).DIM()
+            Fct(wg=wg, w=width, h=height).DIM()
 
             wg.setFrameShape(QtWidgets.QFrame.NoFrame)
 
