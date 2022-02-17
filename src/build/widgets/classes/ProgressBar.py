9119ce1e99bde7118a7fef26ad45bd592f3654f3
LaM0uette <lam0uette.dev@gmail.com>
2022-02-17 15:29:28 +0100

    REMAKE - UPGRADE - ProgressBar

--- src/build/widgets/classes/ProgressBar.py
+++ src/build/widgets/classes/ProgressBar.py
@@ -1,34 +1,42 @@
-
+from .StyleSheet import StyleSheet
 from ....build import *
 from ....build.widgets import p_base
 
 class Style:
     def __init__(self,
                  *wgs,
-                 dim_width=p_base.WG_WIDTH,
-                 dim_height=p_base.WG_HEIGHT,
-                 police=p_base.FONT,
-                 police_taille=p_base.FONT_SIZE,
-                 text_visible=p_base.TEXT_VISIBLE
+                 width=p_base.WG_WIDTH,
+                 height=p_base.WG_HEIGHT,
+                 font=p_base.FONT,
+                 font_size=p_base.FONT_SIZE,
+                 text_visible=p_base.TEXT_VISIBLE,
+                 curseur=P_cur().main(),
+                 style=StyleSheet().get()
     ):
         for wg in wgs:
             wg.setStyleSheet(style)
 
-            try:
-                Fct(wg=wg, w=dim_width, h=dim_height).DIM()
-                wg.setFont(Fct(font=police, font_size=police_taille).FONT())
+            Fct(wg=wg, w=width, h=height).DIM()
+            wg.setFont(Fct(font=font, font_size=font_size).FONT())
 
-                wg.setAlignment(P_align().c().c())
-                wg.setTextVisible(text_visible)
-            except: pass
+            wg.setAlignment(P_align().c().c())
+            wg.setTextVisible(text_visible)
+
+            wg.setCursor(Fct(cur=curseur).CUR())
 
 
 class Base_th(Style):
     def __init__(self, *wgs):
-        super().__init__(*wgs)
+        super().__init__(
+            *wgs,
+            style=StyleSheet(
+            ).get()
+        )
 class Base_tr(Style):
     def __init__(self, *wgs):
         super().__init__(*wgs,
-              couleur_bg=(0, 0, 0, 0),
-              text_visible=False
+              text_visible=False,
+            style=StyleSheet(
+                bg_gen=Rgb().tr()
+            ).get()
     )
