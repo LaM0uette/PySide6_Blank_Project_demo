996c780c407f102ba6f767bbacb4662c74ce9118
LaM0uette <lam0uette.dev@gmail.com>
2022-02-17 10:10:31 +0100

    REMAKE - UPGRADE - ComboBox

--- src/build/widgets/classes/ComboBox.py
+++ src/build/widgets/classes/ComboBox.py
@@ -1,3 +1,4 @@
+from .StyleSheet import StyleSheet
 from ....build import *
 from ....build.widgets import p_base
 
@@ -16,36 +17,14 @@ class Style:
             font=p_base.FONT,
             font_size=p_base.FONT_SIZE,
 
-            # Images
-
-            # Images RGB
-
-            # Images DIM
-            img_width=p_base.IMG_WIDTH,
-            img_height=p_base.IMG_HEIGHT,
-
-            # Bordures GEN
-            border_gen_all=None,
-            border_gen_style=None,
-            border_gen_rgb=None,
-            border_gen_top=None, border_gen_bottom=None, border_gen_right=None, border_gen_left=None,
-            # Bordures
-            border_all=None,
-            border_style=p_base.WG_BORDER_STYLE,
-            border_rgb=p_base.WG_BORDER_RGB,
-            border_top=p_base.WG_BORDER_WIDTH, border_bottom=p_base.WG_BORDER_WIDTH, border_right=p_base.WG_BORDER_WIDTH, border_left=p_base.WG_BORDER_WIDTH,
-            # Bordures hover
-            border_all_hover=None,
-            border_style_hover=p_base.WG_BORDER_STYLE,
-            border_rgb_hover=p_base.WG_BORDER_RGB,
-            border_top_hover=p_base.WG_BORDER_WIDTH, border_bottom_hover=p_base.WG_BORDER_WIDTH, border_right_hover=p_base.WG_BORDER_WIDTH, border_left_hover=p_base.WG_BORDER_WIDTH,
-
-
             # Paramètres
             edit=p_base.EDIT,
 
             # Curseur
-            curseur=p_base.CUR
+            curseur=p_base.CUR,
+
+            # Style
+            style=StyleSheet().get()
     ):
         for wg in wgs:
             wg.setStyleSheet(style)
@@ -58,8 +37,8 @@ class Style:
             wg.setCursor(Fct(cur=curseur).CUR())
             wg.view().setCursor(Fct(cur=P_cur().souris_main()).CUR())
             if edit:
-                    wg.lineEdit().setFont(Fct(font_size=font_size).FONT())
-                    wg.lineEdit().setCursor(Fct(cur=P_cur().IBeam()).CUR())
+                wg.lineEdit().setFont(Fct(font_size=font_size).FONT())
+                wg.lineEdit().setCursor(Fct(cur=P_cur().IBeam()).CUR())
 
 
 class Base_th(Style):
@@ -67,14 +46,18 @@ class Base_th(Style):
         super().__init__(
             *wgs,
             curseur=P_cur().main(),
+            style=StyleSheet(
+            ).get()
     )
 class Base_tr(Style):
     def __init__(self, *wgs):
         super().__init__(
             *wgs,
-            bg_gen=Rgb().tr(),
-            fg=Rgb().th3(),
-            bg_selection=Rgb().th3(),
-            fg_selection=Rgb().th1(),
-            edit=True
+            edit=True,
+            style=StyleSheet(
+                bg_gen=Rgb().tr(),
+                fg=Rgb().th3(),
+                bg_selection=Rgb().th3(),
+                fg_selection=Rgb().th1(),
+            ).get()
     )
