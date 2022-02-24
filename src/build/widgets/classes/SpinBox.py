4fa30439d0a527bbb4aa31c49744ec0e769b769a
LaM0uette <LaM0uette.dev@gmail.com>
2022-02-24 19:20:09 +0100

    REMAKE - UPGRADE - SpinBox

--- src/build/widgets/classes/SpinBox.py
+++ src/build/widgets/classes/SpinBox.py
@@ -124,24 +124,21 @@ class Plus_moins_inf_bd_th3(Style):
 
 """
 "lr":
-                   QSpinBox::up-button, QDoubleSpinBox::up-button  {{
-                    subcontrol-origin: margin;
-                    subcontrol-position: center right;
-                    right: {(dim_h - (dim_h  * P_style().x_ico())) / 2}px;
-                    image: url({pb_up + tm + ".svg"});
-                    height: {dim_h * P_style().x_ico() / 1.6}px;
-                    width: {dim_h * P_style().x_ico() / 1.6}px;
-                    }}
+QSpinBox::up-button, QDoubleSpinBox::up-button  {{
+subcontrol-origin: margin;
+subcontrol-position: center right;
+right: {(dim_h - (dim_h  * P_style().x_ico())) / 2}px;
+image: url({pb_up + tm + ".svg"});
+height: {dim_h * P_style().x_ico() / 1.6}px;
+width: {dim_h * P_style().x_ico() / 1.6}px;
+}}
 
-                    QSpinBox::down-button, QDoubleSpinBox::down-button  {{
-                    subcontrol-origin: margin;
-                    subcontrol-position: center left;
-                    left: {(dim_h - (dim_h  * P_style().x_ico())) / 2}px;
-                    image: url({pb_down + tm + ".svg"});
-                    height: {dim_h * P_style().x_ico() / 1.6}px;
-                    width: {dim_h * P_style().x_ico() / 1.6}px;
-                    }} 
-
-                "tb": 
-
+QSpinBox::down-button, QDoubleSpinBox::down-button  {{
+subcontrol-origin: margin;
+subcontrol-position: center left;
+left: {(dim_h - (dim_h  * P_style().x_ico())) / 2}px;
+image: url({pb_down + tm + ".svg"});
+height: {dim_h * P_style().x_ico() / 1.6}px;
+width: {dim_h * P_style().x_ico() / 1.6}px;
+}}
 """
