18d1a8558257facc383aaed6f1b7bc5e0d5d68c2
LaM0uette <lam0uette.dev@gmail.com>
2022-02-24 09:49:09 +0100

    REMAKE - UPGRADE - slider

--- src/build/widgets/classes/Slider.py
+++ src/build/widgets/classes/Slider.py
@@ -209,15 +209,15 @@ class Base_rond(Style):
             margin_handle_h=(4, -4, 0, 0),
             margin_handle_v=(0, 0, -4, -4),
 
-            border_handle_h=(0, 4, 4, 4),
+            border_handle_h=(0, 10, 2, 2),
             border_handle_h_rgb=Rgb().th2(),
             border_handle_v=(4, 4, 4, 4),
             border_handle_v_rgb=Rgb().th2(),
 
-            radius_groove_h=3,
-            radius_groove_v=3,
-            radius_handle_h=4,
-            radius_handle_v=4,
+            radius_groove_h=(3, 3, 3, 3),
+            radius_groove_v=(3, 3, 3, 3),
+            radius_handle_h=(0, 0, 1, 1),
+            radius_handle_v=(0, 0, 4, 4),
         )
 class rgb(Style):
     def __init__(self,
