a3f78a73effc66710630eab5ba27eee5ff31e036
LaM0uette <LaM0uette.dev@gmail.com>
2022-02-24 19:27:12 +0100

    REMAKE - UPGRADE - SpinBox

--- src/build/widgets/classes/StyleSheet.py
+++ src/build/widgets/classes/StyleSheet.py
@@ -17,8 +17,6 @@ class StyleSheet:
             bg_checked_hover=p_base.BG_CHECKED_HOVER,
             bg_pressed=p_base.BG_PRESSED,
             bg_checked_pressed=p_base.BG_CHECKED_PRESSED,
-            bg_selection=p_base.BG_SELECTION,
-            bg_item_gen=None,
             # Couleurs FG
             fg_gen=None,
             fg=p_base.FG,
@@ -27,52 +25,25 @@ class StyleSheet:
             fg_checked_hover=p_base.FG_CHECKED_HOVER,
             fg_pressed=p_base.FG_PRESSED,
             fg_checked_pressed=p_base.FG_CHECKED_PRESSED,
-            fg_selection=p_base.FG_SELECTION,
-            fg_item_gen=None,
 
             # Images
             img_uncheck=p_base.IMG_UNCHECK,
             img_uncheck_hover=p_base.IMG_UNCHECK_HOVER,
             img_check=p_base.IMG_CHECK,
             img_check_hover=p_base.IMG_CHECK_HOVER,
-            img_all=None,
             img=p_base.IMG_UNROLL,
             img_hover=p_base.IMG_UNROLL_HOVER,
-            img_up=p_base.IMG_UP,
-            img_down=p_base.IMG_DOWN,
             # Images RGB
             img_uncheck_rgb=p_base.IMG_UNCHECK_RGB,
             img_uncheck_hover_rgb=p_base.IMG_UNCHECK_HOVER_RGB,
             img_check_rgb=p_base.IMG_CHECK_RGB,
             img_check_hover_rgb=p_base.IMG_CHECK_HOVER_RGB,
-            img_all_rgb=None,
             img_rgb=p_base.IMG_UNROLL_RGB,
             img_hover_rgb=p_base.IMG_UNROLL_HOVER_RGB,
-            img_up_rgb=p_base.IMG_UP_RGB,
-            img_down_rgb=p_base.IMG_DOWN_RGB,
             # Images DIM
             img_height=p_base.img_height,
             IMG_HEIGHT=p_base.IMG_HEIGHT,
-            img_up_width=10,
-            img_up_height=10,
-            img_down_width=10,
-            img_down_height=10,
 
-            # Images positions
-            img_up_top=2,
-            img_up_bottom=0,
-            img_up_right=2,
-            img_up_left=0,
-            img_down_top=0,
-            img_down_bottom=2,
-            img_down_right=2,
-            img_down_left=0,
-
-            # Bordures GEN
-            border_gen_all=None,
-            border_gen_style=None,
-            border_gen_rgb=None,
-            border_gen_top=None, border_gen_bottom=None, border_gen_right=None, border_gen_left=None,
             # Bordures
             border_all=None,
             border_style=p_base.WG_BORDER_STYLE,
@@ -95,9 +66,6 @@ class StyleSheet:
             border_top_checked_hover=p_base.WG_BORDER_WIDTH, border_bottom_checked_hover=p_base.WG_BORDER_WIDTH, border_right_checked_hover=p_base.WG_BORDER_WIDTH, border_left_checked_hover=p_base.WG_BORDER_WIDTH,
     ):
         self.style = f"""
-/****************************
-**       QPushButton       **
-*****************************/
                 QPushButton {{
                 background-color: rgba{bg};
                 color: rgba{fg};
@@ -152,11 +120,7 @@ class StyleSheet:
                 border-bottom: {border_bottom_checked_hover}px {border_checked_hover_style} rgba{border_checked_hover_rgb};
                 border-right: {border_right_checked_hover}px {border_checked_hover_style} rgba{border_checked_hover_rgb};
                 border-left: {border_left_checked_hover}px {border_checked_hover_style} rgba{border_checked_hover_rgb};
-                }}
-/****************************
-**      /QPushButton       **
-*****************************/
-"""
+                }}"""
         self.img_height = img_height
         self.IMG_HEIGHT = IMG_HEIGHT
         self.img = img
