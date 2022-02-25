980dc46e0995f59399658d07e89a3fb0baba1105
LaM0uette <lam0uette.dev@gmail.com>
2022-02-25 15:37:12 +0100

    REMAKE - UPGRADE - Tray icon

--- src/build/functions.py
+++ src/build/functions.py
@@ -105,8 +105,8 @@ class Fct:
         rgb = hex_colors.lstrip('#')
         lv = len(rgb)
         return tuple(int(rgb[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
-    def QACTION(self, slf, txt, shortcut, fct):
-        return QtGui.QAction(slf, text=txt, shortcut=shortcut, triggered=fct)
+    def QACTION(self, slf, ico, txt, shortcut, fct):
+        return QtGui.QAction(slf, icon=ico, text=txt, shortcut=shortcut, triggered=fct)
 
 
 ### FICHIERS
