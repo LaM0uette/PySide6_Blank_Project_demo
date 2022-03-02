1a726bb688d260db2f8bef42285d6e78054aa67b
LaM0uette <lam0uette.dev@gmail.com>
2022-03-02 14:38:18 +0100

    Création TRAY - ui

---
+++ src/gui/Dlg/dialogs/TrayUi/TrayUi.py
@@ -0,0 +1,74 @@
+from PySide6 import QtCore, QtWidgets, QtGui
+
+from src.gui.Dlg import tray_ui_ui
+from src.build import *
+from src.config import *
+
+
+class TrayUi(tray_ui_ui.Ui_TrayUi, QtWidgets.QDialog):
+    def __init__(self):
+        super(TrayUi, self).__init__()
+
+        self.width = 600
+        self.height = 400
+        self.opacity = 1
+
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        self.setWindowModality(QtCore.Qt.ApplicationModal)
+
+        self.setupUi(self)
+        self.INIT()
+
+
+    ############################
+    ##     INITIALISATION     ##
+    ############################
+    def IN_BASE(self):
+        # Fenetre
+        self.setFixedWidth(self.width)
+        self.setFixedHeight(self.height)
+        self.setWindowOpacity(self.opacity)
+    def IN_CLASSE(self):
+        ### QFrame ###
+        Frame.SplashScreen(self.fr_main)
+        ### /QFrame ###
+
+
+        ### QLabel ###
+        Label.Base_tr(self.lb_titre, font_size=Font().h3())
+        Label.Base_tr(self.lb_description, self.lb_chargement, font_size=Font().h5())
+        ### /QLabel ###
+
+
+        ### QProgressBar ###
+        ProgressBar.Chargement(self.pg_chargement)
+        ### /QProgressBar ###
+    def IN_WG(self):
+        # Base
+        self.setCursor(Fct(cur=Cur().souris()).CUR())
+
+        # Icone de l'app
+        dim = Dim().h5()
+        Fct(wg=self.lb_ico, w=dim, h=dim).DIM()
+        self.lb_ico.setPixmap(QtGui.QPixmap(f"{Img().main()}th2.svg"))
+        self.lb_ico.setScaledContents(True)
+
+        self.lb_titre.setText(config.nom)
+        self.lb_description.setText(config.description)
+    def IN_CONNECTIONS(self):
+        pass
+    def IN_ACT(self):
+        pass
+    def IN_WG_BASE(self):
+        pass
+    def INIT(self):
+        self.IN_BASE()
+        self.IN_CLASSE()
+        self.IN_WG()
+        self.IN_CONNECTIONS()
+        self.IN_ACT()
+        self.IN_WG_BASE()
+    ############################
+    ##    /INITIALISATION     ##
+    ############################
---
+++ src/gui/Dlg/dialogs/TrayUi/__init__.py
@@ -0,0 +1,1 @@
+from src.gui.Dlg.dialogs.TrayUi.TrayUi import TrayUi
