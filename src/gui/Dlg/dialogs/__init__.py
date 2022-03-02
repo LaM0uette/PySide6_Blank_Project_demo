d8429b612d17d94ff395e33da0496fe85a7fd783
LaM0uette <lam0uette.dev@gmail.com>
2022-03-02 10:20:58 +0100

    creation fenetre chargement

--- src/gui/Dlg/dialogs/__init__.py
+++ src/gui/Dlg/dialogs/__init__.py
@@ -3,3 +3,4 @@ from src.gui.Dlg.dialogs.Msg import *
 from src.gui.Dlg.dialogs.Option import *
 from src.gui.Dlg.dialogs.Rep import *
 from src.gui.Dlg.dialogs.Rgb import *
+from src.gui.Dlg.dialogs.SplashScreen import *
