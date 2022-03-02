14f762a1cdf428f8e91e7f53ff1e99a2c81493f0
LaM0uette <lam0uette.dev@gmail.com>
2022-03-02 15:30:25 +0100

    Création TRAY - ui

--- src/gui/Dlg/tray_ui_ui.py
+++ src/gui/Dlg/tray_ui_ui.py
@@ -15,8 +15,8 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QFrame, QSizePolicy, QSpacerItem,
-    QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QFrame, QHBoxLayout, QPushButton,
+    QSizePolicy, QSpacerItem, QVBoxLayout, QWidget)
 
 class Ui_TrayUi(object):
     def setupUi(self, TrayUi):
@@ -34,10 +34,40 @@ class Ui_TrayUi(object):
         self.vlay_fr_main = QVBoxLayout(self.fr_main)
         self.vlay_fr_main.setSpacing(0)
         self.vlay_fr_main.setObjectName(u"vlay_fr_main")
-        self.vlay_fr_main.setContentsMargins(5, 5, 5, 5)
+        self.vlay_fr_main.setContentsMargins(0, 0, 0, 0)
+        self.fr_menu_top = QFrame(self.fr_main)
+        self.fr_menu_top.setObjectName(u"fr_menu_top")
+        self.hlay_menu_top = QHBoxLayout(self.fr_menu_top)
+        self.hlay_menu_top.setSpacing(0)
+        self.hlay_menu_top.setObjectName(u"hlay_menu_top")
+        self.hlay_menu_top.setContentsMargins(0, 0, 0, 0)
+        self.pb_mt_pin = QPushButton(self.fr_menu_top)
+        self.pb_mt_pin.setObjectName(u"pb_mt_pin")
+
+        self.hlay_menu_top.addWidget(self.pb_mt_pin)
+
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.hlay_menu_top.addItem(self.horizontalSpacer)
+
+        self.pb_mt_quitter = QPushButton(self.fr_menu_top)
+        self.pb_mt_quitter.setObjectName(u"pb_mt_quitter")
+
+        self.hlay_menu_top.addWidget(self.pb_mt_quitter)
+
+
+        self.vlay_fr_main.addWidget(self.fr_menu_top)
+
+        self.fr_body = QVBoxLayout()
+        self.fr_body.setSpacing(0)
+        self.fr_body.setObjectName(u"fr_body")
+        self.fr_body.setContentsMargins(5, 5, 5, 5)
         self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
 
-        self.vlay_fr_main.addItem(self.verticalSpacer)
+        self.fr_body.addItem(self.verticalSpacer)
+
+
+        self.vlay_fr_main.addLayout(self.fr_body)
 
 
         self.verticalLayout.addWidget(self.fr_main)
