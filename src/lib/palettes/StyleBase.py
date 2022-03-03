7ea6eebfaaffe6061dce8dc555ae4496ac45a32c
LaM0uette <LaM0uette.dev@gmail.com>
2022-03-03 18:04:45 +0100

    REMAKE 100% - config palettes

--- src/lib/palettes/StyleBase.py
+++ src/lib/palettes/StyleBase.py
@@ -1,9 +1,7 @@
-from src.build.palettes.data.Data import Data
 
-class StyleBase(Data):
-    def __init__(self, grp="wg"):
-        super().__init__(grp)
 
-    def bd(self): return self.GET_VAL("bd")
-    def x_ico(self): return self.GET_VAL("x_ico")
-    def X_ICO(self): return self.GET_VAL("X_ICO")
+class StyleBase:
+
+    def bd(self): return 2
+    def x_ico(self): return 0.65
+    def X_ICO(self): return 0.80
