a10b9d38642fd2817ccccfdc66b5b4f4493ac857
LaM0uette <lam0uette.dev@gmail.com>
2022-03-03 15:03:08 +0100

    REMAKE 100% - config file

--- src/config/config.py
+++ src/config/config.py
@@ -21,6 +21,6 @@ cur = cfg["config"]["cur"]
 
 ### var _____________________
 debug = cfg["var"]["debug"].lower() == "true"
-auto_close = cfg["var"]["autoclose"].lower() == "true"
 resize = cfg["var"]["resize"].lower() == "true"
-tray_ui_pin = cfg["var"]["tray_ui_pin"].lower() == "true"
+autoClose = cfg["var"]["autoclose"].lower() == "true"
+toolBoxPin = cfg["var"]["tray_ui_pin"].lower() == "true"
