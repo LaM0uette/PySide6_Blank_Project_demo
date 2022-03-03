7750eb2de59eb031937da0342ccd2d1061a5052d
LaM0uette <LaM0uette.dev@gmail.com>
2022-03-03 17:50:53 +0100

    REMAKE 100% - config palettes

--- src/lib/palettes/Dim.py
+++ src/lib/palettes/Dim.py
@@ -1,18 +1,15 @@
-from src.build.palettes.data.Data import Data
 
-class Dim(Data):
-    def __init__(self, grp="dimension"):
-        super().__init__(grp)
 
-    def h0(self): return self.GET_VAL("h0")
-    def h1(self): return self.GET_VAL("h1")
-    def h2(self): return self.GET_VAL("h2")
-    def h3(self): return self.GET_VAL("h3")
-    def h4(self): return self.GET_VAL("h4")
-    def h5(self): return self.GET_VAL("h5")
-    def h6(self): return self.GET_VAL("h6")
-    def h7(self): return self.GET_VAL("h7")
-    def h8(self): return self.GET_VAL("h8")
-    def h9(self): return self.GET_VAL("h9")
-    def h10(self): return self.GET_VAL("h10")
+class Dim:
 
+    def h0(self): return 800
+    def h1(self): return 600
+    def h2(self): return 500
+    def h3(self): return 400
+    def h4(self): return 300
+    def h5(self): return 200
+    def h6(self): return 100
+    def h7(self): return 75
+    def h8(self): return 50
+    def h9(self): return 25
+    def h10(self): return 20
