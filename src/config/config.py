95fc19045c7ec338e1be5d356ba4b0725d8f9b57
LaM0uette <lam0uette.dev@gmail.com>
2022-03-03 15:43:48 +0100

    REMAKE 100% - config file

--- src/config/config.py
+++ src/config/config.py
@@ -3,25 +3,25 @@ import os
 
 
 ### FICHIER CONFIG ______________
-with open(os.path.relpath("src/config/config.json"), "r", encoding="utf-8") as fichier:
+with open(os.path.abspath(r"src\config\config.json"), "r", encoding="utf-8") as fichier:
     cfg = json.load(fichier)
 
 ### infos _______________
 nom = cfg["nom"]
 description = cfg["description"]
-version = float(cfg["version"])
+version = cfg["version"]
 auteur = cfg["auteur"]
 
 ### config ________________________
 theme = cfg["config"]["theme"]
 font = cfg["config"]["font"]
-widht = int(cfg["config"]["widht"])
-height = int(cfg["config"]["height"])
-opacity = float(cfg["config"]["opacity"])
+widht = cfg["config"]["widht"]
+height = cfg["config"]["height"]
+opacity = cfg["config"]["opacity"]
 cur = cfg["config"]["cur"]
 
 ### var _____________________
-debug = cfg["var"]["debug"].lower() == "true"
-resize = cfg["var"]["resize"].lower() == "true"
-autoClose = cfg["var"]["autoClose"].lower() == "true"
-toolBoxPin = cfg["var"]["toolBoxPin"].lower() == "true"
+debug = cfg["var"]["debug"]
+resize = cfg["var"]["resize"]
+autoClose = cfg["var"]["autoClose"]
+toolBoxPin = cfg["var"]["toolBoxPin"]
