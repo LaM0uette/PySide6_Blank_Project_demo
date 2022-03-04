dabf5cfdef94202339f6685f7413f01dea0f38cf
LaM0uette <lam0uette.dev@gmail.com>
2022-03-04 16:17:47 +0100

    REMAKE 100% - tray icon actions

--- src/build/mods/Functions.py
+++ src/build/mods/Functions.py
@@ -8,7 +8,7 @@ class Functions:
             if width is not None: wg.setFixedWidth(width)
             if height is not None: wg.setFixedHeight(height)
 
-    def ADD_QACTION(self, tray, ico=None, ico_rgb=None, txt="", status_tip="", height=None, fct=None, sht_1=None, sht_2=None, sht_3=None):
+    def ADD_QACTION(self, tray, ico=None, ico_rgb=None, txt="", shortcut_txt="", status_tip="", height=None, fct=None, sht_1=None, sht_2=None, sht_3=None):
         if height is None: height=12
 
         shortcut = int()
@@ -19,7 +19,7 @@ class Functions:
         action = QtGui.QAction(self)
         action.setIcon(QtGui.QPixmap(f"{ico}{ico_rgb}.svg").scaledToHeight(height))
         action.setText(txt)
-        action.setShortcut("Ctrl+Shift+A")
+        action.setShortcut(shortcut_txt)
         action.setStatusTip(status_tip)
         action.triggered.connect(fct)
 
