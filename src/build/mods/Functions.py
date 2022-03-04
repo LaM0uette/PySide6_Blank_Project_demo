37593bf798af76d41897239850e041c2a5558592
LaM0uette <lam0uette.dev@gmail.com>
2022-03-04 16:16:42 +0100

    REMAKE 100% - tray icon actions

--- src/build/mods/Functions.py
+++ src/build/mods/Functions.py
@@ -8,20 +8,20 @@ class Functions:
             if width is not None: wg.setFixedWidth(width)
             if height is not None: wg.setFixedHeight(height)
 
-    def ADD_QACTION(self, tray, ico=None, ico_rgb=None, txt="", height=None, fct=None):
+    def ADD_QACTION(self, tray, ico=None, ico_rgb=None, txt="", status_tip="", height=None, fct=None, sht_1=None, sht_2=None, sht_3=None):
         if height is None: height=12
 
-        action = QtGui.QAction(self)
-        action.setIcon(QtGui.QPixmap(f"{ico}{ico_rgb}.svg").scaledToHeight(height))
-        action.setText(txt)
-        action.setShortcut(QtGui.QKeySequence(QtCore.Qt.SHIFT + QtCore.Qt.Key_Escape))
-        action.triggered.connect(fct)
-
-        tray.addAction(action)
-    def ADD_QSHORTCUT(self, slf, sht_1, sht_2, sht_3, fct):
         shortcut = int()
         if sht_1 is not None: shortcut += sht_1
         if sht_2 is not None: shortcut += sht_2
         if sht_3 is not None: shortcut += sht_3
 
-        QtGui.QShortcut(QtGui.QKeySequence(shortcut), slf).activated.connect(fct)
+        action = QtGui.QAction(self)
+        action.setIcon(QtGui.QPixmap(f"{ico}{ico_rgb}.svg").scaledToHeight(height))
+        action.setText(txt)
+        action.setShortcut("Ctrl+Shift+A")
+        action.setStatusTip(status_tip)
+        action.triggered.connect(fct)
+
+        tray.addAction(action)
+        QtGui.QShortcut(QtGui.QKeySequence(shortcut), self).activated.connect(fct)
