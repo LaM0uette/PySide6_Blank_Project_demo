c2a21cadcec3c40cd5535829d2f176b73512be75
LaM0uette <LaM0uette.dev@gmail.com>
2022-03-06 14:23:20 +0100

    REMAKE 100% - wg - DateEdit

---
+++ src/widgets/DateEdit/DateEdit.py
@@ -0,0 +1,238 @@
+from PySide6 import QtCore
+
+from src.lib.palettes import *
+from src.widgets import vb_wg
+from src.widgets.DateEdit.Build import Build
+
+class Style:
+    def __init__(
+            self,
+            *wgs,
+            width=VBase.WG_WIDTH,
+            height=VBase.WG_HEIGHT,
+            font=VBase.FONT,
+            font_size=VBase.FONT_SIZE,
+            align_horizontal=Align().h_center(),
+            align_vertical=Align().v_center(),
+            curseur=Cur().souris_main(),
+
+            # Couleurs BG
+            bg=VBase.BG,
+            bg_hover=VBase.BG_HOVER,
+            bg_selection=VBase.BG_SELECTION,
+            bg_item=VBase.BG_ITEM,
+            bg_item_hover=VBase.BG_ITEM_HOVER,
+            bg_header=Rgb().th2(),
+            bg_header_hover=Rgb().th2(),
+            bg_mois=Rgb().th2(),
+            # Couleurs FG
+            fg=VBase.FG,
+            fg_hover=VBase.FG_HOVER,
+            fg_selection=VBase.FG_SELECTION,
+            fg_item=VBase.FG_ITEM,
+            fg_item_hover=VBase.FG_ITEM_HOVER,
+            fg_header=Rgb().th1(),
+            fg_header_hover=Rgb().bn1(),
+            fg_mois=Rgb().th1(),
+
+            # Images
+            img=VBase.IMG_UNROLL,
+            img_hover=VBase.IMG_UNROLL_HOVER,
+            img_right=VBase.IMG_RIGHT,
+            img_left=VBase.IMG_LEFT,
+            # Images RGB
+            img_rgb=VBase.IMG_UNROLL_RGB,
+            img_hover_rgb=VBase.IMG_UNROLL_HOVER_RGB,
+            img_right_rgb=VBase.IMG_RIGHT_RGB,
+            img_left_rgb=VBase.IMG_LEFT_RGB,
+            # Images DIM
+            img_width=VBase.img_width,
+            img_height=VBase.img_height,
+            # Images positions
+            img_margin=((0,) * 4),
+
+            # Bordures
+            border=VBase.WG_BORDER_WIDTH,
+            border_style=VBase.WG_BORDER_STYLE,
+            border_rgb=VBase.WG_BORDER_RGB,
+            # Bordures hover
+            border_hover=VBase.WG_BORDER_WIDTH,
+            border_hover_style=VBase.WG_BORDER_STYLE,
+            border_hover_rgb=VBase.WG_BORDER_RGB,
+
+            # Bordures jours
+            border_day_size=StyleBase().bd(),
+            border_day_style=VBase.WG_BORDER_STYLE,
+            border_day_rgb=VBase.FG_ITEM_HOVER,
+
+            # Rayons
+            radius=VBase.WG_RADIUS,
+    ):
+        style = f"""
+                /* WIDGET */
+                QDateEdit {{
+                background-color: rgba{bg};
+                color: rgba{fg};
+                selection-background-color: rgba{bg_selection};
+                selection-color: rgba{fg_selection};
+                }}
+                QDateEdit:hover {{
+                background-color: rgba{bg_hover};
+                color: rgba{fg_hover};
+                }}
+
+                /* IMG CALENDRIER */
+                QDateEdit::drop-down {{
+                image: url({f"{img}{img_rgb}.svg"});
+                width: {img_width}px;
+                height: {img_height}px;
+                margin-top: {img_margin[0]}px;
+                margin-bottom: {img_margin[1]}px;
+                margin-right: {img_margin[2]}px;
+                margin-left: {img_margin[3]}px;
+                }}
+                QDateEdit::drop-down:hover {{
+                image: url({f"{img_hover}{img_hover_rgb}.svg"});
+                }}
+
+                /* WIDGETS */
+                QCalendarWidget QWidget {{
+                alternate-background-color: rgba{bg_mois};
+                color: rgb{fg_mois};
+                }}
+
+                /* TOOL BUTTON */
+                QCalendarWidget QToolButton {{
+                font-size: {font_size}px;
+                background-color: rgba{bg_header};
+                color: rgba{fg_header};
+                }}
+                QCalendarWidget QToolButton:hover {{
+                background-color: rgba{bg_header_hover};
+                color: rgba{fg_header_hover};
+                }}
+
+                /* FLECHE GAUCHE DROITE */
+                QToolButton#qt_calendar_nextmonth  {{
+                qproperty-icon: url({f"{img_right}{img_right_rgb}.svg"});
+                icon-size: {font_size}px, {font_size}px;
+                }}
+                QToolButton#qt_calendar_prevmonth {{
+                qproperty-icon: url({f"{img_left}{img_left_rgb}.svg"});
+                icon-size: {font_size}px, {font_size}px;
+                }}
+
+                /* MENU DEROULANT */
+                QCalendarWidget QMenu {{
+                width: 150px;
+                font-size: {font_size}px;
+                font-family: {font};
+                background-color: rgba{bg_header};
+                color: rgba{fg_header};
+                }}
+
+                /* SPIN BOX */
+                QCalendarWidget QSpinBox {{
+                width: 60px;
+                font-size: {font_size}px;
+                font-family: {font};
+                background-color: rgba{bg_header};
+                color: rgba{fg_header};
+                selection-background-color: rgba{bg_selection};
+                selection-color: rgba{fg_selection};
+                }}
+
+                /* JOURS */
+                QCalendarWidget QAbstractItemView {{
+                font-size: {font_size}px;
+                font-family: {font};
+                font-weight: 30;
+                outline: 0px;
+                }}
+                QCalendarWidget QAbstractItemView:enabled {{
+                background-color: rgba{bg_item};
+                color: rgba{fg_item};
+                selection-background-color: rgba{fg_item};
+                selection-color: rgba{bg_item};
+                }}
+                QCalendarWidget QWidget:item:hover, QCalendarWidget QWidget:item:selected {{
+                background-color: rgba{bg_item_hover};
+                color: rgba{fg_item_hover};
+                border: {border_day_size}px {border_day_style} rgba{border_day_rgb};
+                }}
+
+                /* BARRE HAUT */
+                QCalendarWidget QWidget#qt_calendar_navigationbar {{
+                background-color: rgba{bg_header};
+                }}
+
+                /* BORDURES */
+                .QDateEdit {{
+                border-top: {border[0]}px {border_style} rgba{border_rgb};
+                border-bottom: {border[1]}px {border_style} rgba{border_rgb};
+                border-right: {border[2]}px {border_style} rgba{border_rgb};
+                border-left: {border[3]}px {border_style} rgba{border_rgb};
+                }}
+                .QDateEdit:hover {{
+                border-top: {border_hover[0]}px {border_hover_style} rgba{border_hover_rgb};
+                border-bottom: {border_hover[1]}px {border_hover_style} rgba{border_hover_rgb};
+                border-right: {border_hover[2]}px {border_hover_style} rgba{border_hover_rgb};
+                border-left: {border_hover[3]}px {border_hover_style} rgba{border_hover_rgb};
+                }}
+                
+                /* RAYONS */
+                .QDateEdit {{
+                border-top-right-radius: {radius[0]}px;
+                border-top-left-radius: {radius[1]}px;
+                border-bottom-right-radius: {radius[2]}px;
+                border-bottom-left-radius: {radius[3]}px;
+                }}"""
+
+        for wg in wgs:
+            wg.setStyleSheet(style)
+
+            wg.setCalendarPopup(True)
+            dateDuJour = vrb.DATE_NOW_FORMAT.split("_")
+            QdateDuJour = QtCore.QDate(int(dateDuJour[2]), int(dateDuJour[1]), int(dateDuJour[0]))
+            wg.setDateTime(QtCore.QDateTime(QdateDuJour, QtCore.QTime(0, 0, 0)))
+            wg.setDate(QdateDuJour)
+            wg.setFocusPolicy(QtCore.Qt.NoFocus)
+
+            Fct(wg=wg, w=width, h=height).DIM()
+            wg.setFont(Fct(font=font, font_size=font_size).FONT())
+
+            wg.setAlignment(align_horizontal | align_vertical)
+
+            wg.setCursor(Fct(cur=curseur).CUR())
+            wg.lineEdit().setCursor(Fct(cur=Cur().IBeam()).CUR())
+            wg.calendarWidget().setCursor(Fct(cur=Cur().souris_main()).CUR())
+
+
+##################
+##     BASE     ##
+##################
+class Base_th(Style):
+    def __init__(self, *wgs):
+        super().__init__(
+            *wgs,
+            img=Img().calendrier(),
+            img_hover=Img().calendrier(),
+            img_rgb="",
+            img_hover_rgb="",
+            img_margin=((VBase.WG_HEIGHT - VBase.IMG_HEIGHT) / 2, 0, (VBase.WG_HEIGHT - VBase.IMG_HEIGHT) / 2, 0),
+        )
+class Base_tr(Style):
+    def __init__(self, *wgs):
+        super().__init__(
+            *wgs,
+            bg=Rgb().tr(),
+            bg_hover=Rgb().tr(),
+            fg=Rgb().th3(),
+            bg_selection=Rgb().th3(),
+            fg_selection=Rgb().th1(),
+            img=Img().calendrier(),
+            img_hover=Img().calendrier(),
+            img_rgb="",
+            img_hover_rgb="",
+            img_margin=((VBase.WG_HEIGHT - VBase.IMG_HEIGHT) / 2, 0, (VBase.WG_HEIGHT - VBase.IMG_HEIGHT) / 2, 0),
+    )
--- src/widgets/__init__.py
+++ src/widgets/__init__.py
@@ -1,3 +1,4 @@
 from src.widgets.CheckBox import CheckBox
 from src.widgets.ComboBox import ComboBox
+from src.widgets.DateEdit import DateEdit
 from src.widgets.RadioButton import RadioButton
