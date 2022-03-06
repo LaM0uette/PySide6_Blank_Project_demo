1971d6383360795bdfa01dd7b08af4a454072e14
LaM0uette <LaM0uette.dev@gmail.com>
2022-03-06 14:26:59 +0100

    REMAKE 100% - wg - DateEdit

--- src/widgets/DateEdit/Build.py
+++ src/widgets/DateEdit/Build.py
@@ -6,45 +6,46 @@ class Build:
     def __init__(
             self,
             *wgs,
-
-            # Dimensions
             width=vb_wg.WIDTH,
             height=vb_wg.HEIGHT,
-
-            # Police
             font=vb_wg.FONT,
             font_size=vb_wg.FONT_SIZE,
-
-            # Paramètres
-            edit=vb_wg.EDIT,
-            max_visible_items=vb_wg.MAX_VISIBLE_ITEMS,
-            insert_policy=vb_wg.INSERT_POLICY,
-            curseur=vb_wg.CUR_MAIN,
-            curseur_view=vb_wg.CUR_VIEW,
-
+            align_horizontal=Align().h_center(),
+            align_vertical=Align().v_center(),
+            curseur=Cur().souris_main(),
             # Couleurs BG
             bg=vb_wg.BG,
             bg_hover=vb_wg.BG_HOVER,
             bg_selection=vb_wg.BG_SELECTION,
             bg_item=vb_wg.BG_ITEM,
             bg_item_hover=vb_wg.BG_ITEM_HOVER,
+            bg_header=Rgb().th2(),
+            bg_header_hover=Rgb().th2(),
+            bg_mois=Rgb().th2(),
             # Couleurs FG
             fg=vb_wg.FG,
             fg_hover=vb_wg.FG_HOVER,
             fg_selection=vb_wg.FG_SELECTION,
             fg_item=vb_wg.FG_ITEM,
             fg_item_hover=vb_wg.FG_ITEM_HOVER,
-
+            fg_header=Rgb().th1(),
+            fg_header_hover=Rgb().bn1(),
+            fg_mois=Rgb().th1(),
             # Images
             img=vb_wg.IMG_UNROLL,
             img_hover=vb_wg.IMG_UNROLL_HOVER,
+            img_right=vb_wg.IMG_RIGHT,
+            img_left=vb_wg.IMG_LEFT,
             # Images RGB
             img_rgb=vb_wg.IMG_UNROLL_RGB,
             img_hover_rgb=vb_wg.IMG_UNROLL_HOVER_RGB,
+            img_right_rgb=vb_wg.IMG_RIGHT_RGB,
+            img_left_rgb=vb_wg.IMG_LEFT_RGB,
             # Images DIM
             img_width=vb_wg.img_width,
             img_height=vb_wg.img_height,
-
+            # Images positions
+            img_margin=((0,) * 4),
             # Bordures
             border=vb_wg.BORDER_WIDTH,
             border_style=vb_wg.BORDER_STYLE,
@@ -53,124 +54,119 @@ class Build:
             border_hover=vb_wg.BORDER_WIDTH,
             border_hover_style=vb_wg.BORDER_STYLE,
             border_hover_rgb=vb_wg.BORDER_RGB,
-
+            # Bordures jours
+            border_day_size=StyleBase().bd(),
+            border_day_style=vb_wg.BORDER_STYLE,
+            border_day_rgb=vb_wg.FG_ITEM_HOVER,
             # Rayons
             radius=vb_wg.RADIUS,
-
-            # Scroll
-            scroll_bg=vb_wg.SCROLL_BG,
-            scroll_width=vb_wg.SCROLL_WIDTH,
-            scroll_height=vb_wg.SCROLL_HEIGHT,
-            scroll_handle_bg=vb_wg.SCROLL_HANDLE_BG,
-            scroll_handle_bg_hover=vb_wg.SCROLL_HANDLE_BG_HOVER,
-            scroll_handle_fg=vb_wg.SCROLL_HANDLE_FG,
-            scroll_handle_fg_hover=vb_wg.SCROLL_HANDLE_FG_HOVER,
-            scroll_handle_min_width=vb_wg.SCROLL_HANDLE_MIN_WIDTH,
-            scroll_handle_min_height=vb_wg.SCROLL_HANDLE_MIN_HEIGHT,
     ):
-        """
-        *Border_Style: str() : dashed | dot-dash | dot-dot-dash | dotted | double | groove | inset | outset | ridge | solid | none \n
-        *Cur: list() : Cur().%nomCurseur() \n
-        *Dim: int() : Dim().%nomDim() \n
-        *Font: int() : Font().%nomFont() \n
-        *Img: str() : Img().%nomImage() \n
-        *InsertPolicy: QtWidgets.QComboBox.%policy : InsertPolicy().%nomPolicy() \n
-        *Rgb: tuple() : Rgb().%nomCouleur() \n
-        *Tuple: tuple() : (int(), int(), int(), int()) == (Top, Bottom, Right, Left) | (TopRight, TopLeft, BottomRight, BottomLeft) \n
-
-        :param wgs: Widgets séparés par ","
-        :param width: *Dim
-        :param height: *Dim
-        :param font: str()
-        :param font_size: *Font
-        :param edit: bool()
-        :param setMaxVisibleItems: int()
-        :param setInsertPolicy: *InsertPolicy
-        :param curseur: *Cur
-        :param curseur_view: *Cur
-        :param bg: *Rgb
-        :param bg_hover: *Rgb
-        :param bg_selection: *Rgb
-        :param bg_item: *Rgb
-        :param bg_item_hover: *Rgb
-        :param fg: *Rgb
-        :param fg_hover: *Rgb
-        :param fg_selection: *Rgb
-        :param fg_item: *Rgb
-        :param fg_item_hover: *Rgb
-        :param img: *Img
-        :param img_hover: *Img
-        :param img_rgb: *Rgb
-        :param img_hover_rgb:
-        :param img_width: *Dim
-        :param img_height: *Dim
-        :param border: *Tuplev
-        :param border_style: *Border_Style
-        :param border_rgb: *Rgb
-        :param border_hover: *Tuple
-        :param border_hover_style: *Border_Style
-        :param border_hover_rgb:
-        :param radius: *Tuple
-        :param scroll_bg: *Rgb
-        :param scroll_width: *Dim
-        :param scroll_height: *Dim
-        :param scroll_handle_bg: *Rgb
-        :param scroll_handle_bg_hover: *Rgb
-        :param scroll_handle_fg: *Rgb
-        :param scroll_handle_fg_hover: *Rgb
-        :param scroll_handle_min_width: *Dim
-        :param scroll_handle_min_height: *Dim
-        """
-
         style = f"""
-                /* COMBOBOX */
-                QComboBox, QFontComboBox {{
+                /* WIDGET */
+                QDateEdit {{
                 background-color: rgba{bg};
                 color: rgba{fg};
                 selection-background-color: rgba{bg_selection};
                 selection-color: rgba{fg_selection};
                 }}
-                QComboBox:hover, QFontComboBox:hover {{
+                QDateEdit:hover {{
                 background-color: rgba{bg_hover};
                 color: rgba{fg_hover};
                 }}
 
-                /* BOUTON DE DEROULEMENT */
-                QComboBox::drop-down, QFontComboBox::drop-down {{
-                width: {height}px;
-                border: none;
-                }}
-
-                /* IMAGE DU BOUTON DE DEROULEMENT */
-                QComboBox::down-arrow, QFontComboBox::down-arrow {{
+                /* IMG CALENDRIER */
+                QDateEdit::drop-down {{
                 image: url({f"{img}{img_rgb}.svg"});
                 width: {img_width}px;
                 height: {img_height}px;
+                margin-top: {img_margin[0]}px;
+                margin-bottom: {img_margin[1]}px;
+                margin-right: {img_margin[2]}px;
+                margin-left: {img_margin[3]}px;
                 }}
-                QComboBox::down-arrow:hover, QFontComboBox::down-arrow:hover {{
+                QDateEdit::drop-down:hover {{
                 image: url({f"{img_hover}{img_hover_rgb}.svg"});
-                width: {img_width}px;
-                height: {img_height}px;
                 }}
 
-                /* ELEMENTS DEROULEMENT */
-                QComboBox QAbstractItemView::item, QFontComboBox QAbstractItemView::item {{
+                /* WIDGETS */
+                QCalendarWidget QWidget {{
+                alternate-background-color: rgba{bg_mois};
+                color: rgb{fg_mois};
+                }}
+
+                /* TOOL BUTTON */
+                QCalendarWidget QToolButton {{
+                font-size: {font_size}px;
+                background-color: rgba{bg_header};
+                color: rgba{fg_header};
+                }}
+                QCalendarWidget QToolButton:hover {{
+                background-color: rgba{bg_header_hover};
+                color: rgba{fg_header_hover};
+                }}
+
+                /* FLECHE GAUCHE DROITE */
+                QToolButton#qt_calendar_nextmonth  {{
+                qproperty-icon: url({f"{img_right}{img_right_rgb}.svg"});
+                icon-size: {font_size}px, {font_size}px;
+                }}
+                QToolButton#qt_calendar_prevmonth {{
+                qproperty-icon: url({f"{img_left}{img_left_rgb}.svg"});
+                icon-size: {font_size}px, {font_size}px;
+                }}
+
+                /* MENU DEROULANT */
+                QCalendarWidget QMenu {{
+                width: 150px;
+                font-size: {font_size}px;
+                font-family: {font};
+                background-color: rgba{bg_header};
+                color: rgba{fg_header};
+                }}
+
+                /* SPIN BOX */
+                QCalendarWidget QSpinBox {{
+                width: 60px;
+                font-size: {font_size}px;
+                font-family: {font};
+                background-color: rgba{bg_header};
+                color: rgba{fg_header};
+                selection-background-color: rgba{bg_selection};
+                selection-color: rgba{fg_selection};
+                }}
+
+                /* JOURS */
+                QCalendarWidget QAbstractItemView {{
+                font-size: {font_size}px;
+                font-family: {font};
+                font-weight: 30;
+                outline: 0px;
+                }}
+                QCalendarWidget QAbstractItemView:enabled {{
                 background-color: rgba{bg_item};
                 color: rgba{fg_item};
+                selection-background-color: rgba{fg_item};
+                selection-color: rgba{bg_item};
                 }}
-                QComboBox QAbstractItemView::item:hover, QFontComboBox QAbstractItemView::item:hover {{
+                QCalendarWidget QWidget:item:hover, QCalendarWidget QWidget:item:selected {{
                 background-color: rgba{bg_item_hover};
                 color: rgba{fg_item_hover};
+                border: {border_day_size}px {border_day_style} rgba{border_day_rgb};
+                }}
+
+                /* BARRE HAUT */
+                QCalendarWidget QWidget#qt_calendar_navigationbar {{
+                background-color: rgba{bg_header};
                 }}
 
                 /* BORDURES */
-                .QComboBox, .QFontComboBox {{
+                .QDateEdit {{
                 border-top: {border[0]}px {border_style} rgba{border_rgb};
                 border-bottom: {border[1]}px {border_style} rgba{border_rgb};
                 border-right: {border[2]}px {border_style} rgba{border_rgb};
                 border-left: {border[3]}px {border_style} rgba{border_rgb};
                 }}
-                .QComboBox:hover, .QFontComboBox:hover {{
+                .QDateEdit:hover {{
                 border-top: {border_hover[0]}px {border_hover_style} rgba{border_hover_rgb};
                 border-bottom: {border_hover[1]}px {border_hover_style} rgba{border_hover_rgb};
                 border-right: {border_hover[2]}px {border_hover_style} rgba{border_hover_rgb};
@@ -178,60 +174,28 @@ class Build:
                 }}
 
                 /* RAYONS */
-                .QComboBox, .QFontComboBox {{
+                .QDateEdit {{
                 border-top-right-radius: {radius[0]}px;
                 border-top-left-radius: {radius[1]}px;
                 border-bottom-right-radius: {radius[2]}px;
                 border-bottom-left-radius: {radius[3]}px;
-                }}
-
-                /* SCROLL */
-                QScrollBar {{
-                background-color: rgba{scroll_bg};
-                width: {scroll_width}px;
-                height: {scroll_height}px;
-                }}
-                QScrollBar::handle:horizontal {{
-                min-width: {scroll_handle_min_width}px;
-                }}
-                QScrollBar::handle:vertical {{
-                min-height: {scroll_handle_min_height}px;
-                }}
-                QScrollBar::handle {{
-                background-color: rgba{scroll_handle_fg};
-                }}
-                QScrollBar::handle:hover {{
-                background-color: rgba{scroll_handle_fg_hover};
-                }}
-
-                QScrollBar::add-page, QScrollBar::sub-page {{
-                background-color: rgba{scroll_handle_bg};
-                border: none;
-                }}
-                QScrollBar::add-page:hover, QScrollBar::sub-page:hover {{
-                background-color: rgba{scroll_handle_bg_hover};
-                border: none;
                 }}"""
+
         for wg in wgs:
-            # Dimensions
-            Functions().SET_DIM(wg, width=width, height=height)
-
-            # Police
-            Functions().SET_FONT(wg, font=font, font_size=font_size)
-
-            # Paramètres
-            wg.setEditable(edit)
-            wg.setMaxVisibleItems(max_visible_items)
-            wg.setInsertPolicy(insert_policy)
-
-            if edit:
-                Functions().SET_FONT(wg, font=font, font_size=font_size)
-                wg.lineEdit().setFont(Functions().SET_FONT(wg, font=font, font_size=font_size, rtn=True))
-                wg.lineEdit().setCursor(Functions().SET_CURSOR(vb_wg.CUR_LE))
-
-            # Curseur
-            wg.setCursor(Functions().SET_CURSOR(curseur))
-            wg.view().setCursor(Functions().SET_CURSOR(curseur_view))
-
-            # Style
             wg.setStyleSheet(style)
+
+            wg.setCalendarPopup(True)
+            dateDuJour = vrb.DATE_NOW_FORMAT.split("_")
+            QdateDuJour = QtCore.QDate(int(dateDuJour[2]), int(dateDuJour[1]), int(dateDuJour[0]))
+            wg.setDateTime(QtCore.QDateTime(QdateDuJour, QtCore.QTime(0, 0, 0)))
+            wg.setDate(QdateDuJour)
+            wg.setFocusPolicy(QtCore.Qt.NoFocus)
+
+            Fct(wg=wg, w=width, h=height).DIM()
+            wg.setFont(Fct(font=font, font_size=font_size).FONT())
+
+            wg.setAlignment(align_horizontal | align_vertical)
+
+            wg.setCursor(Fct(cur=curseur).CUR())
+            wg.lineEdit().setCursor(Fct(cur=Cur().IBeam()).CUR())
+            wg.calendarWidget().setCursor(Fct(cur=Cur().souris_main()).CUR())
