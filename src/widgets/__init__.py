3e8c4e36912de6c9fd558226ddd282c7e1c038ff
LaM0uette <LaM0uette.dev@gmail.com>
2022-03-06 19:50:52 +0100

    REMAKE 100% - wg - Label

--- src/widgets/__init__.py
+++ src/widgets/__init__.py
@@ -2,5 +2,6 @@ from src.widgets.CheckBox import CheckBox
 from src.widgets.ComboBox import ComboBox
 from src.widgets.DateEdit import DateEdit
 from src.widgets.Frame import Frame
+from src.widgets.Label import Label
 from src.widgets.RadioButton import RadioButton
 from src.widgets.ScrollArea import ScrollArea
