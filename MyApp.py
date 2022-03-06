8d8397cdea4b9e43e375bcdafd1fd078d247d6ff
LaM0uette <LaM0uette.dev@gmail.com>
2022-03-06 20:42:51 +0100

    REMAKE 100% - wg - Label

--- MyApp.py
+++ MyApp.py
@@ -131,7 +131,7 @@ class main(Ui_main, QtWidgets.QWidget):
 
         ### Widget blanc pour centrer le nom de l'app ###
         dim = Dim().h9() * 1.4
-        Functions().SET_DIM(self.wg_mt_blank, width=dim * 3, height=dim)
+        Functions().SET_DIM(self.wg_mt_blank, width=dim*3, height=dim)
 
 
         ### Version de l'app ###
