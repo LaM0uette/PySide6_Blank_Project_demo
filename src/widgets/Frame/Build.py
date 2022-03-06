52aed50d434d5445da81103f8cb97d34484cfaab
LaM0uette <LaM0uette.dev@gmail.com>
2022-03-06 16:41:32 +0100

    REMAKE 100% - wg - Frame

--- src/widgets/Frame/Build.py
+++ src/widgets/Frame/Build.py
@@ -64,8 +64,7 @@ class Build:
             Functions().SET_DIM(wg, width=width, height=height)
 
             # Paramètres
-            if shadow is not None:
-                wg.setGraphicsEffect(shadow)
+            if shadow is not None: wg.setGraphicsEffect(shadow)
 
             # Curseur
             wg.setCursor(Functions().SET_CURSOR(curseur))
