2d69ae1c81fbb01ae03dc6f6e5f8cd76bc135385
LaM0uette <LaM0uette.dev@gmail.com>
2022-03-09 19:09:08 +0100

    REMAKE 100% - UI - DLG_Option

--- src/build/mods/Json.py
+++ src/build/mods/Json.py
@@ -11,13 +11,13 @@ class Json:
             return json.load(fichier)
     def WRITE(self, data):
         with open(self.lien_json, 'w') as fichier:
-            json.dump(data, fichier)
+            json.dump(data, fichier, indent=4)
     def UPDATE(self, dct):
         with open(self.lien_json, "r+") as fichier:
             data = json.load(fichier)
             data.update(dct)
             fichier.seek(0)
-            json.dump(data, fichier)
+            json.dump(data, fichier, indent=4)
             fichier.truncate()
     def REMOVE(self, key):
         with open(self.lien_json, "r+") as fichier:
