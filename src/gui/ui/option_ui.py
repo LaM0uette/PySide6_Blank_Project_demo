2d461cbe49b184a9ed166fb4ea5a93cd82b216ef
LaM0uette <lam0uette.dev@gmail.com>
2022-03-09 16:05:56 +0100

    REMAKE 100% - UI - DLG_Option

--- src/gui/ui/option_ui.py
+++ src/gui/ui/option_ui.py
@@ -17,19 +17,19 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QCheckBox, QComboBox, QFontComboBox,
     QFrame, QGridLayout, QHBoxLayout, QHeaderView,
-    QLabel, QLineEdit, QPushButton, QSizePolicy,
-    QSpacerItem, QSpinBox, QStackedWidget, QTreeWidget,
-    QTreeWidgetItem, QVBoxLayout, QWidget)
+    QLabel, QPushButton, QSizePolicy, QSpacerItem,
+    QSpinBox, QStackedWidget, QTreeWidget, QTreeWidgetItem,
+    QVBoxLayout, QWidget)
 
 class Ui_Option(object):
     def setupUi(self, Option):
         if not Option.objectName():
             Option.setObjectName(u"Option")
         Option.resize(972, 625)
-        self.glay_dlg = QGridLayout(Option)
-        self.glay_dlg.setSpacing(0)
-        self.glay_dlg.setObjectName(u"glay_dlg")
-        self.glay_dlg.setContentsMargins(10, 10, 10, 10)
+        self.glay_main = QGridLayout(Option)
+        self.glay_main.setSpacing(0)
+        self.glay_main.setObjectName(u"glay_main")
+        self.glay_main.setContentsMargins(0, 0, 0, 0)
         self.fr_main = QFrame(Option)
         self.fr_main.setObjectName(u"fr_main")
         self.fr_main.setFrameShape(QFrame.StyledPanel)
@@ -81,7 +81,6 @@ class Ui_Option(object):
         QTreeWidgetItem(self.trw_option)
         QTreeWidgetItem(self.trw_option)
         QTreeWidgetItem(self.trw_option)
-        QTreeWidgetItem(self.trw_option)
         self.trw_option.setObjectName(u"trw_option")
 
         self.hlay_opt_body.addWidget(self.trw_option)
@@ -91,152 +90,17 @@ class Ui_Option(object):
         self.pg_opt_menu = QWidget()
         self.pg_opt_menu.setObjectName(u"pg_opt_menu")
         self.stk_option.addWidget(self.pg_opt_menu)
-        self.pg_opt_font = QWidget()
-        self.pg_opt_font.setObjectName(u"pg_opt_font")
-        self.vlay_pg_opt_font = QVBoxLayout(self.pg_opt_font)
-        self.vlay_pg_opt_font.setSpacing(10)
-        self.vlay_pg_opt_font.setObjectName(u"vlay_pg_opt_font")
-        self.vlay_pg_opt_font.setContentsMargins(10, 20, 10, 10)
-        self.fcb_opt_ft_font = QFontComboBox(self.pg_opt_font)
-        self.fcb_opt_ft_font.setObjectName(u"fcb_opt_ft_font")
-
-        self.vlay_pg_opt_font.addWidget(self.fcb_opt_ft_font)
-
-        self.verticalSpacer_12 = QSpacerItem(20, 20, QSizePolicy.Minimum, QSizePolicy.Fixed)
-
-        self.vlay_pg_opt_font.addItem(self.verticalSpacer_12)
-
-        self.fr_opt_ft_h1 = QFrame(self.pg_opt_font)
-        self.fr_opt_ft_h1.setObjectName(u"fr_opt_ft_h1")
-        self.hlay_opt_ft_h1 = QHBoxLayout(self.fr_opt_ft_h1)
-        self.hlay_opt_ft_h1.setSpacing(10)
-        self.hlay_opt_ft_h1.setObjectName(u"hlay_opt_ft_h1")
-        self.hlay_opt_ft_h1.setContentsMargins(5, 5, 5, 5)
-        self.lb_opt_ft_h1 = QLabel(self.fr_opt_ft_h1)
-        self.lb_opt_ft_h1.setObjectName(u"lb_opt_ft_h1")
-
-        self.hlay_opt_ft_h1.addWidget(self.lb_opt_ft_h1)
-
-        self.sb_opt_ft_h1 = QSpinBox(self.fr_opt_ft_h1)
-        self.sb_opt_ft_h1.setObjectName(u"sb_opt_ft_h1")
-
-        self.hlay_opt_ft_h1.addWidget(self.sb_opt_ft_h1)
-
-        self.le_opt_ft_texte_h1 = QLineEdit(self.fr_opt_ft_h1)
-        self.le_opt_ft_texte_h1.setObjectName(u"le_opt_ft_texte_h1")
-
-        self.hlay_opt_ft_h1.addWidget(self.le_opt_ft_texte_h1)
-
-
-        self.vlay_pg_opt_font.addWidget(self.fr_opt_ft_h1)
-
-        self.fr_opt_ft_h2 = QFrame(self.pg_opt_font)
-        self.fr_opt_ft_h2.setObjectName(u"fr_opt_ft_h2")
-        self.hlay_opt_ft_h2 = QHBoxLayout(self.fr_opt_ft_h2)
-        self.hlay_opt_ft_h2.setSpacing(10)
-        self.hlay_opt_ft_h2.setObjectName(u"hlay_opt_ft_h2")
-        self.hlay_opt_ft_h2.setContentsMargins(5, 5, 5, 5)
-        self.lb_opt_ft_h2 = QLabel(self.fr_opt_ft_h2)
-        self.lb_opt_ft_h2.setObjectName(u"lb_opt_ft_h2")
-
-        self.hlay_opt_ft_h2.addWidget(self.lb_opt_ft_h2)
-
-        self.sb_opt_ft_h2 = QSpinBox(self.fr_opt_ft_h2)
-        self.sb_opt_ft_h2.setObjectName(u"sb_opt_ft_h2")
-
-        self.hlay_opt_ft_h2.addWidget(self.sb_opt_ft_h2)
-
-        self.le_opt_ft_texte_h2 = QLineEdit(self.fr_opt_ft_h2)
-        self.le_opt_ft_texte_h2.setObjectName(u"le_opt_ft_texte_h2")
-
-        self.hlay_opt_ft_h2.addWidget(self.le_opt_ft_texte_h2)
-
-
-        self.vlay_pg_opt_font.addWidget(self.fr_opt_ft_h2)
-
-        self.fr_opt_ft_h3 = QFrame(self.pg_opt_font)
-        self.fr_opt_ft_h3.setObjectName(u"fr_opt_ft_h3")
-        self.hlay_opt_ft_h3 = QHBoxLayout(self.fr_opt_ft_h3)
-        self.hlay_opt_ft_h3.setSpacing(10)
-        self.hlay_opt_ft_h3.setObjectName(u"hlay_opt_ft_h3")
-        self.hlay_opt_ft_h3.setContentsMargins(5, 5, 5, 5)
-        self.lb_opt_ft_h3 = QLabel(self.fr_opt_ft_h3)
-        self.lb_opt_ft_h3.setObjectName(u"lb_opt_ft_h3")
-
-        self.hlay_opt_ft_h3.addWidget(self.lb_opt_ft_h3)
-
-        self.sb_opt_ft_h3 = QSpinBox(self.fr_opt_ft_h3)
-        self.sb_opt_ft_h3.setObjectName(u"sb_opt_ft_h3")
-
-        self.hlay_opt_ft_h3.addWidget(self.sb_opt_ft_h3)
-
-        self.le_opt_ft_texte_h3 = QLineEdit(self.fr_opt_ft_h3)
-        self.le_opt_ft_texte_h3.setObjectName(u"le_opt_ft_texte_h3")
-
-        self.hlay_opt_ft_h3.addWidget(self.le_opt_ft_texte_h3)
-
-
-        self.vlay_pg_opt_font.addWidget(self.fr_opt_ft_h3)
-
-        self.fr_opt_ft_h4 = QFrame(self.pg_opt_font)
-        self.fr_opt_ft_h4.setObjectName(u"fr_opt_ft_h4")
-        self.hlay_opt_ft_h4 = QHBoxLayout(self.fr_opt_ft_h4)
-        self.hlay_opt_ft_h4.setSpacing(10)
-        self.hlay_opt_ft_h4.setObjectName(u"hlay_opt_ft_h4")
-        self.hlay_opt_ft_h4.setContentsMargins(5, 5, 5, 5)
-        self.lb_opt_ft_h4 = QLabel(self.fr_opt_ft_h4)
-        self.lb_opt_ft_h4.setObjectName(u"lb_opt_ft_h4")
-
-        self.hlay_opt_ft_h4.addWidget(self.lb_opt_ft_h4)
-
-        self.sb_opt_ft_h4 = QSpinBox(self.fr_opt_ft_h4)
-        self.sb_opt_ft_h4.setObjectName(u"sb_opt_ft_h4")
-
-        self.hlay_opt_ft_h4.addWidget(self.sb_opt_ft_h4)
-
-        self.le_opt_ft_texte_h4 = QLineEdit(self.fr_opt_ft_h4)
-        self.le_opt_ft_texte_h4.setObjectName(u"le_opt_ft_texte_h4")
-
-        self.hlay_opt_ft_h4.addWidget(self.le_opt_ft_texte_h4)
-
-
-        self.vlay_pg_opt_font.addWidget(self.fr_opt_ft_h4)
-
-        self.fr_opt_ft_h5 = QFrame(self.pg_opt_font)
-        self.fr_opt_ft_h5.setObjectName(u"fr_opt_ft_h5")
-        self.hlay_opt_ft_h5 = QHBoxLayout(self.fr_opt_ft_h5)
-        self.hlay_opt_ft_h5.setSpacing(10)
-        self.hlay_opt_ft_h5.setObjectName(u"hlay_opt_ft_h5")
-        self.hlay_opt_ft_h5.setContentsMargins(5, 5, 5, 5)
-        self.lb_opt_ft_h5 = QLabel(self.fr_opt_ft_h5)
-        self.lb_opt_ft_h5.setObjectName(u"lb_opt_ft_h5")
-
-        self.hlay_opt_ft_h5.addWidget(self.lb_opt_ft_h5)
-
-        self.sb_opt_ft_h5 = QSpinBox(self.fr_opt_ft_h5)
-        self.sb_opt_ft_h5.setObjectName(u"sb_opt_ft_h5")
-
-        self.hlay_opt_ft_h5.addWidget(self.sb_opt_ft_h5)
-
-        self.le_opt_ft_texte_h5 = QLineEdit(self.fr_opt_ft_h5)
-        self.le_opt_ft_texte_h5.setObjectName(u"le_opt_ft_texte_h5")
-
-        self.hlay_opt_ft_h5.addWidget(self.le_opt_ft_texte_h5)
-
-
-        self.vlay_pg_opt_font.addWidget(self.fr_opt_ft_h5)
-
-        self.verticalSpacer_13 = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
-
-        self.vlay_pg_opt_font.addItem(self.verticalSpacer_13)
-
-        self.stk_option.addWidget(self.pg_opt_font)
         self.pg_opt_configs = QWidget()
         self.pg_opt_configs.setObjectName(u"pg_opt_configs")
         self.vlay_pg_opt_configs = QVBoxLayout(self.pg_opt_configs)
         self.vlay_pg_opt_configs.setSpacing(10)
         self.vlay_pg_opt_configs.setObjectName(u"vlay_pg_opt_configs")
         self.vlay_pg_opt_configs.setContentsMargins(10, 20, 10, 10)
+        self.fcb_opt_ft_font = QFontComboBox(self.pg_opt_configs)
+        self.fcb_opt_ft_font.setObjectName(u"fcb_opt_ft_font")
+
+        self.vlay_pg_opt_configs.addWidget(self.fcb_opt_ft_font)
+
         self.fr_opt_cfg_opacity = QFrame(self.pg_opt_configs)
         self.fr_opt_cfg_opacity.setObjectName(u"fr_opt_cfg_opacity")
         self.hlay_opt_fr_opt_cfg_opacity = QHBoxLayout(self.fr_opt_cfg_opacity)
@@ -448,7 +312,7 @@ class Ui_Option(object):
         self.hlay_opt_bottom = QHBoxLayout(self.fr_opt_bottom)
         self.hlay_opt_bottom.setSpacing(2)
         self.hlay_opt_bottom.setObjectName(u"hlay_opt_bottom")
-        self.hlay_opt_bottom.setContentsMargins(0, 2, 0, 0)
+        self.hlay_opt_bottom.setContentsMargins(0, 2, 1, 1)
         self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
 
         self.hlay_opt_bottom.addItem(self.horizontalSpacer)
@@ -467,12 +331,12 @@ class Ui_Option(object):
         self.vlay_fr_main.addWidget(self.fr_opt_bottom)
 
 
-        self.glay_dlg.addWidget(self.fr_main, 1, 0, 1, 1)
+        self.glay_main.addWidget(self.fr_main, 1, 0, 1, 1)
 
 
         self.retranslateUi(Option)
 
-        self.stk_option.setCurrentIndex(2)
+        self.stk_option.setCurrentIndex(1)
 
 
         QMetaObject.connectSlotsByName(Option)
@@ -483,29 +347,17 @@ class Ui_Option(object):
         __sortingEnabled = self.trw_option.isSortingEnabled()
         self.trw_option.setSortingEnabled(False)
         ___qtreewidgetitem = self.trw_option.topLevelItem(0)
-        ___qtreewidgetitem.setText(0, QCoreApplication.translate("Option", u"Polices", None));
+        ___qtreewidgetitem.setText(0, QCoreApplication.translate("Option", u"Configs", None));
         ___qtreewidgetitem1 = self.trw_option.topLevelItem(1)
-        ___qtreewidgetitem1.setText(0, QCoreApplication.translate("Option", u"Configs", None));
+        ___qtreewidgetitem1.setText(0, QCoreApplication.translate("Option", u"Th\u00e8mes", None));
         ___qtreewidgetitem2 = self.trw_option.topLevelItem(2)
-        ___qtreewidgetitem2.setText(0, QCoreApplication.translate("Option", u"Th\u00e8mes", None));
-        ___qtreewidgetitem3 = self.trw_option.topLevelItem(3)
-        ___qtreewidgetitem3.setText(0, QCoreApplication.translate("Option", u"Infos", None));
+        ___qtreewidgetitem2.setText(0, QCoreApplication.translate("Option", u"Infos", None));
         self.trw_option.setSortingEnabled(__sortingEnabled)
 
-        self.lb_opt_ft_h1.setText(QCoreApplication.translate("Option", u"Taille H1: ", None))
-        self.le_opt_ft_texte_h1.setText(QCoreApplication.translate("Option", u"TEST De la Police", None))
-        self.lb_opt_ft_h2.setText(QCoreApplication.translate("Option", u"Taille H2: ", None))
-        self.le_opt_ft_texte_h2.setText(QCoreApplication.translate("Option", u"TEST De la Police", None))
-        self.lb_opt_ft_h3.setText(QCoreApplication.translate("Option", u"Taille H3: ", None))
-        self.le_opt_ft_texte_h3.setText(QCoreApplication.translate("Option", u"TEST De la Police", None))
-        self.lb_opt_ft_h4.setText(QCoreApplication.translate("Option", u"Taille H4: ", None))
-        self.le_opt_ft_texte_h4.setText(QCoreApplication.translate("Option", u"TEST De la Police", None))
-        self.lb_opt_ft_h5.setText(QCoreApplication.translate("Option", u"Taille H5: ", None))
-        self.le_opt_ft_texte_h5.setText(QCoreApplication.translate("Option", u"TEST De la Police", None))
         self.lb_opt_cfg_opacity.setText(QCoreApplication.translate("Option", u"Opacit\u00e9 ", None))
         self.lb_opt_cfg_autoclose.setText(QCoreApplication.translate("Option", u"Auto close : ", None))
         self.lb_opt_cfg_debug.setText(QCoreApplication.translate("Option", u"Debug : ", None))
-        self.lb_opt_cfg_ui_pin.setText(QCoreApplication.translate("Option", u"Ui pin :", None))
+        self.lb_opt_cfg_ui_pin.setText(QCoreApplication.translate("Option", u"ToolBox Pin :", None))
         self.lb_opt_cfg_resize.setText(QCoreApplication.translate("Option", u"Manuel : ", None))
         self.lb_opt_cfg_resize_width.setText(QCoreApplication.translate("Option", u"Largeur : ", None))
         self.lb_opt_cfg_resize_height.setText(QCoreApplication.translate("Option", u"Hauteur : ", None))
