3c38f04ba6832395d37806505f6cb98f78b22d1d
LaM0uette <lam0uette.dev@gmail.com>
2022-03-11 14:03:29 +0100

    REMAKE 100% - UI - Upgrade option - correction bug reload

--- src/widgets/CheckBox/CheckBox.py
+++ src/widgets/CheckBox/CheckBox.py
@@ -1,4 +1,5 @@
 import importlib
+import time
 
 from src.lib.palettes import *
 from src.widgets import vb_wg
@@ -49,12 +50,6 @@ class Demo:
         self.margin = margin
 
     def th(self):
-        from src.widgets import vb_wg
-        from src.widgets.CheckBox import Build as d
-
-        importlib.reload(vb_wg)
-        importlib.reload(d)
-
         Build(
             *self.wgs,
             img_margin=self.margin,
