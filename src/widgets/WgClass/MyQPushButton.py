882d6114c8811112296e4e30c868d910465067f3
LaM0uette <LaM0uette.dev@gmail.com>
2022-03-13 14:36:58 +0100

    REMAKE - Refonte des WG avec héritage avancé - MyPushButton

---
+++ src/widgets/WgClass/MyQPushButton.py
@@ -0,0 +1,18 @@
+from .MyQAbstractButton import MyQAbstractButton
+from src.build.mods import Functions
+from src.lib.globals import v_wg
+
+
+class MyQPushButton(MyQAbstractButton):
+    def __init__(
+            self,
+            widget,
+            auto_default=False,
+            default=False,
+            flat=True,
+    ):
+        super().__init__(widget)
+
+        widget.setAutoDefault(auto_default)
+        widget.setDefault(default)
+        widget.setFlat(flat)
--- src/widgets/WgClass/__init__.py
+++ src/widgets/WgClass/__init__.py
@@ -1,2 +1,3 @@
 from src.widgets.WgClass.MyQWidget import MyQWidget
 from src.widgets.WgClass.MyQAbstractButton import MyQAbstractButton
+from src.widgets.WgClass.MyQPushButton import MyQPushButton
