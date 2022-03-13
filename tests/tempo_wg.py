f19149d02d9ece73673b7202da817ccbd11835b6
LaM0uette <LaM0uette.dev@gmail.com>
2022-03-13 12:33:06 +0100

    REMAKE 100% - UI - Upgrade option - correction bug reload

---
+++ tests/tempo_wg.py
@@ -0,0 +1,143 @@
+# ### QLineEdit ###
+# LineEdit.Base(self.le_demo_th).th()
+# LineEdit.Base(self.le_demo_tr).tr()
+# ### /QLineEdit ###
+#
+#
+# ### QTextEdit ###
+# TextEdit.Demo(self.te_demo_th).th()
+# TextEdit.Demo(self.te_demo_tr).tr()
+# ### /QTextEdit ###
+#
+#
+# ### QPlainTextEdit ###
+# PlainTextEdit.Demo(self.pte_demo_th).th()
+# PlainTextEdit.Demo(self.pte_demo_tr).tr()
+# ### /QPlainTextEdit ###
+#
+#
+# ### QScrollBoxArea ###
+# ScrollArea.Base(self.sca_main).th()
+# ### /QScrollBoxArea ###
+#
+#
+# ## QCheckBox ###
+# CheckBox.Demo(self.ck_demo_th_1, self.ck_demo_th_2, self.ck_demo_th_3).th()
+# CheckBox.Demo(self.ck_demo_tr_1, self.ck_demo_tr_2, self.ck_demo_tr_3).tr()
+# ## /QCheckBox ###
+#
+#
+# ### QComboBox ###
+# ComboBox.Base(self.cb_demo_th).th()
+# ComboBox.Base(self.cb_demo_tr).tr()
+# ### /QComboBox ###
+#
+#
+# ### QDateEdit ###
+# DateEdit.Base(self.de_demo_th).th()
+# DateEdit.Base(self.de_demo_tr).tr()
+# ### /QDateEdit ###
+#
+#
+# ### QFrame ###
+# Frame.Menu(self.fr_menu_top).top()
+# Frame.Cadre(self.fr_main).th2()
+# Frame.Base(self.fr_body).tr()
+# Frame.Base(self.fr_demo_th, self.fr_tb_demo_th_1, self.fr_tb_demo_th_2).th()
+# Frame.Base(self.fr_tb_demo_tr_1, self.fr_tb_demo_tr_2).th()
+# Frame.Cadre(
+#     self.fr_cb, self.fr_de, self.fr_lw, self.fr_pb, self.fr_ck,
+#     self.fr_rb, self.fr_pg, self.fr_sb, self.fr_tw, self.fr_le,
+#     self.fr_te, self.fr_pte, self.fr_tb, self.fr_trw, self.fr_sd,
+#     self.fr_demo_cadre, self.fr_fr, self.fr_lb, border=(StyleBase().border(), )*4
+# ).bn1()
+# Frame.Menu(self.fr_menu_bottom).bottom()
+# Frame.Demo_hover(self.fr_demo_cadre_hover)
+# ### /QFrame ###
+#
+#
+# ### QLabel ###
+# Label.Base(self.lb_mt_ico).ico_main()
+# Label.Demo(self.lb_lb_demo_th).th()
+# Label.Base(self.lb_mt_nom, font_size=Font().h3()).tr()
+# Label.Base(self.lb_mb_version).tr()
+# Label.Demo(self.lb_lb_demo_tr).tr()
+# Label.Demo(
+#     self.lb_cb_demo, self.lb_de_demo, self.lb_lw_demo, self.lb_pb_demo, self.lb_ck_demo,
+#     self.lb_rb_demo, self.lb_pg_demo, self.lb_sb_demo, self.lb_tw_demo, self.lb_le_demo,
+#     self.lb_te_demo, self.lb_pte_demo, self.lb_tb_demo, self.lb_trw_demo, self.lb_sd_demo,
+#     self.lb_fr_demo, self.lb_lb_demo
+# ).wg_categorie()
+# ### /QLabel ###
+#
+#
+# ### QListWidget ###
+# ListWidget.Demo(self.lv_demo_th, self.lw_demo_th).th()
+# ListWidget.Demo(self.lv_demo_tr, self.lw_demo_tr).tr()
+# ### /QListWidget ###
+#
+#
+# ### QProgressBar ###
+# ProgressBar.Demo(self.pg_demo_th).th()
+# ProgressBar.Demo(self.pg_demo_tr).tr()
+# ### /QProgressBar ###
+#
+#
+# ### QPushButton ###
+# PushButton.Base(self.pb_demo_th, self.pb_demo_ck).th()
+# PushButton.Base(self.pb_demo_tr).tr()
+#
+# PushButton.menu_top(self.pb_mt_option).option()
+# PushButton.menu_top(self.pb_mt_reduire).reduire()
+# PushButton.menu_top(self.pb_mt_agrandir).agrandir()
+# PushButton.menu_top(self.pb_mt_quitter).quitter()
+#
+# PushButton.Txt(self.pb_demo_txt).txt()
+# PushButton.Txt(self.pb_demo_txt_inv).inv()
+#
+# PushButton.ck_ico(self.pb_demo_ck_ico, self.pb_demo_ico_ck)
+# PushButton.zoom(self.pb_demo_zoom)
+# ### /QPushButton ###
+#
+#
+# ## QRadioButton ###
+# RadioButton.Base(self.rb_demo_th_1, self.rb_demo_th_2, self.rb_demo_th_3).th()
+# RadioButton.Base(self.rb_demo_tr_1, self.rb_demo_tr_2, self.rb_demo_tr_3).tr()
+# ## /QRadioButton ###
+#
+#
+# ## QRadioButton ###
+# TrayIcon.Main(self.tray_menu)
+# ## /QRadioButton ###
+#
+#
+# ### QSlider ###
+# Slider.Base(self.hsd_demo).th()
+# Slider.Base(self.vsd_demo).rond()
+# ### /QSlider ###
+#
+#
+# ### QSpinBox ###
+# SpinBox.PlusMinus(self.sb_demo).th()
+# SpinBox.PlusMinus(self.sb_demo_3).tr()
+# SpinBox.UpDown(self.sb_demo_2).th()
+# SpinBox.UpDown(self.dsb_demo).tr()
+# ### /QSpinBox ###
+#
+#
+# ### QTableWidget ###
+# TableWidget.Demo(self.tv_demo_th, self.tw_demo_th).th()
+# TableWidget.Demo(self.tv_demo_tr, self.tw_demo_tr).tr()
+# ### /QTableWidget ###
+#
+#
+# ### QToolBox ###
+# ToolBox.Demo(self.tb_demo_th).th()
+# ToolBox.Demo(self.tb_demo_tr).tr()
+# ### /QToolBox ###
+#
+#
+# ### QTreeWidget ###
+# TreeWidget.Base(self.trv_demo_th, self.trw_demo_th).th()
+# TreeWidget.Base(self.trv_demo_tr, self.trw_demo_tr).tr()
+# ### /QTreeWidget ###
