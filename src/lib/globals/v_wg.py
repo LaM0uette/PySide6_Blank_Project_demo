bbf286faab8242e4cc547b48ea5aa4b7ae77fe64
LaM0uette <LaM0uette.dev@gmail.com>
2022-03-13 15:06:35 +0100

    REMAKE - Refonte des WG avec héritage avancé - MyPushButton

--- src/lib/globals/v_wg.py
+++ src/lib/globals/v_wg.py
@@ -42,7 +42,8 @@ REPEAT_INTERVAL = 100
 ###################
 ##     STYLE     ##
 ###################
-# Background
+
+###  BACKGROUND  ###
 BG = Rgb().th3()
 BG_ALTERNATE = Rgb().th2()
 BG_HOVER = Rgb().th3()
@@ -68,8 +69,10 @@ BG_HANDLE = Rgb().th2()
 BG_HANDLE_HOVER = Rgb().th2()
 BG_HANDLE_PRESSED = Rgb().bn1()
 BG_SEPARATOR = Rgb().bn1()
+### /BACKGROUND  ###
 
-# foreground
+
+###  FOREGROUND  ###
 FG = Rgb().th1()
 FG_HOVER = Rgb().bn1()
 FG_CHECKED = Rgb().th3()
@@ -85,9 +88,51 @@ FG_ITEM = Rgb().th1()
 FG_ITEM_HOVER = Rgb().bn1()
 FG_ITEM_CHECKED = Rgb().th3()
 FG_ITEM_CHECKED_HOVER = Rgb().bn1()
+### /FOREGROUND  ###
 
-# Other
+
+###  OTHER  ###
 GRIDLINE = Rgb().th2()
+### /OTHER  ###
+
+
+###  IMAGES  ###
+IMG_UNCHECK = Img().check0()
+IMG_UNCHECK_HOVER = Img().check0()
+IMG_CHECK = Img().check2()
+IMG_CHECK_HOVER = Img().check2()
+IMG_INDETERMINATE = Img().check1()
+IMG_INDETERMINATE_HOVER = Img().check1()
+IMG_UNROLL = Img().fleche_bottom()
+IMG_UNROLL_HOVER = Img().fleche_bottom()
+    # Fleches
+IMG_UP = Img().plus()
+IMG_DOWN = Img().moins()
+IMG_RIGHT = Img().fleche_droite()
+IMG_LEFT = Img().fleche_gauche()
+### /IMAGES  ###
+
+
+###  IMAGES RGB  ###
+IMG_UNCHECK_RGB = "th2"
+IMG_UNCHECK_HOVER_RGB = "bn1"
+IMG_CHECK_RGB = "th2"
+IMG_CHECK_HOVER_RGB = "bn1"
+IMG_INDETERMINATE_RGB = "th3"
+IMG_INDETERMINATE_HOVER_RGB = "th1"
+IMG_UNROLL_RGB = "th2"
+IMG_UNROLL_HOVER_RGB = "bn1"
+    # Fleches RGB
+IMG_UP_RGB = "th2"
+IMG_UP_HOVER_RGB = "bn1"
+IMG_DOWN_RGB = "th2"
+IMG_DOWN_HOVER_RGB = "bn1"
+IMG_RIGHT_RGB = "th3"
+IMG_RIGHT_HOVER_RGB = "bn1"
+IMG_LEFT_RGB = "th3"
+IMG_LEFT_HOVER_RGB = "bn1"
+### /IMAGES RGB  ###
+
 ###################
 ##    /STYLE     ##
 ###################
