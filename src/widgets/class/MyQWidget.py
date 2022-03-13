d70813c6f4b283f07ceafa422c981eb882d2e984
LaM0uette <LaM0uette.dev@gmail.com>
2022-03-13 12:53:08 +0100

    REMAKE - Refonte des WG avec héritage avancé - MyPushButton

---
+++ src/widgets/class/MyQWidget.py
@@ -0,0 +1,12 @@
+
+
+class MyQWidget:
+    def __init__(
+            self,
+            widget,
+
+            size_policy_h,
+            size_policy_v,
+    ):
+
+        widget.setSizePolicy(size_policy_h, size_policy_v)
