9ce16de8a2b8b8035ff60a80e2ec0619b8906b82
LaM0uette <LaM0uette.dev@gmail.com>
2022-03-13 17:28:12 +0100

    REMAKE - Refonte des WG avec héritage avancé - MyPushButton

--- src/widgets/MyPushButton.py
+++ src/widgets/MyPushButton.py
@@ -73,35 +73,7 @@ class Style(MyQPushButton):
             border_checked_hover_rgb=v_wg.BORDER_RGB,
             radius=v_wg.RADIUS
     ):
-        super().__init__(
-            widget,
-            size_policy_h,
-            size_policy_v,
-            fixed_width,
-            fixed_height,
-            minimum_width,
-            minimum_height,
-            maximum_width,
-            maximum_height,
-            font,
-            cursor,
-            focus_policy,
-            layout_direction,
-            txt,
-            ico,
-            ico_rgb,
-            ico_width,
-            ico_height,
-            checkable,
-            checked,
-            auto_repeat,
-            auto_exclusive,
-            auto_repeat_delay,
-            auto_repeat_interval,
-            auto_default,
-            default,
-            flat
-        )
+        super().__init__(widget,size_policy_h,size_policy_v,fixed_width,fixed_height,minimum_width,minimum_height,maximum_width,maximum_height,font,cursor,focus_policy,layout_direction,txt,ico,ico_rgb,ico_width,ico_height,checkable,checked,auto_repeat,auto_exclusive,auto_repeat_delay,auto_repeat_interval,auto_default,default,flat)
 
         # Style
         style = f"""
