87bad0577152694e8a9ee6d743bc69afed3c2fad
LaM0uette <LaM0uette.dev@gmail.com>
2022-03-13 17:28:54 +0100

    REMAKE - Refonte des WG avec héritage avancé - MyPushButton

--- src/widgets/WgClass/MyQAbstractButton.py
+++ src/widgets/WgClass/MyQAbstractButton.py
@@ -30,21 +30,7 @@ class MyQAbstractButton(MyQWidget):
             auto_repeat_delay,
             auto_repeat_interval
     ):
-        super().__init__(
-            widget,
-            size_policy_h,
-            size_policy_v,
-            fixed_width,
-            fixed_height,
-            minimum_width,
-            minimum_height,
-            maximum_width,
-            maximum_height,
-            font,
-            cursor,
-            focus_policy,
-            layout_direction
-        )
+        super().__init__(widget,size_policy_h,size_policy_v,fixed_width,fixed_height,minimum_width,minimum_height,maximum_width,maximum_height,font,cursor,focus_policy,layout_direction)
 
         # Txt
         widget.setText(txt)
--- src/widgets/WgClass/MyQPushButton.py
+++ src/widgets/WgClass/MyQPushButton.py
@@ -32,32 +32,7 @@ class MyQPushButton(MyQAbstractButton):
             default,
             flat
     ):
-        super().__init__(
-            widget,
-            size_policy_h,
-            size_policy_v,
-            fixed_width,
-            fixed_height,
-            minimum_width,
-            minimum_height,
-            maximum_width,
-            maximum_height,
-            font,
-            cursor,
-            focus_policy,
-            layout_direction,
-            txt,
-            ico,
-            ico_rgb,
-            ico_width,
-            ico_height,
-            checkable,
-            checked,
-            auto_repeat,
-            auto_exclusive,
-            auto_repeat_delay,
-            auto_repeat_interval
-        )
+        super().__init__(widget,size_policy_h,size_policy_v,fixed_width,fixed_height,minimum_width,minimum_height,maximum_width,maximum_height,font,cursor,focus_policy,layout_direction,txt,ico,ico_rgb,ico_width,ico_height,checkable,checked,auto_repeat,auto_exclusive,auto_repeat_delay,auto_repeat_interval)
 
         # Default
         widget.setAutoDefault(auto_default)
