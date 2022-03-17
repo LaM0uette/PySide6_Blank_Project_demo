ed8e6320e7de36f7c8e5732b19e408df3e99b83a
LaM0uette <lam0uette.dev@gmail.com>
2022-03-17 10:58:45 +0100

    REMAKE - Refonte des WG avec héritage avancé - MyToolButton

---
+++ src/widgets/WgClass/MyQToolButton.py
@@ -0,0 +1,42 @@
+from .MyQAbstractButton import MyQAbstractButton
+
+
+class MyQToolButton(MyQAbstractButton):
+    def __init__(
+            self,
+            widget,
+            size_policy_h,
+            size_policy_v,
+            fixed_width,
+            fixed_height,
+            minimum_width,
+            minimum_height,
+            maximum_width,
+            maximum_height,
+            font,
+            cursor,
+            focus_policy,
+            layout_direction,
+            txt,
+            ico,
+            ico_rgb,
+            ico_width,
+            ico_height,
+            checkable,
+            checked,
+            auto_repeat,
+            auto_exclusive,
+            auto_repeat_delay,
+            auto_repeat_interval,
+            popup_mode,
+            tool_button_style,
+            auto_raise,
+            arrow_type,
+    ):
+        super().__init__(widget,size_policy_h,size_policy_v,fixed_width,fixed_height,minimum_width,minimum_height,maximum_width,maximum_height,font,cursor,focus_policy,layout_direction,txt,ico,ico_rgb,ico_width,ico_height,checkable,checked,auto_repeat,auto_exclusive,auto_repeat_delay,auto_repeat_interval)
+
+        # Default
+        widget.setPopupMode(popup_mode)
+        widget.setToolButtonStyle(tool_button_style)
+        widget.setAutoRaise(auto_raise)
+        widget.setArrowType(arrow_type)
