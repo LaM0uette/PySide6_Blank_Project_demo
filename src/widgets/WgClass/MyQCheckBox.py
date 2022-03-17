c50157269c4ce430e8342627ce835bf2ba74a298
LaM0uette <LaM0uette.dev@gmail.com>
2022-03-17 19:42:54 +0100

    REMAKE - Refonte des WG avec héritage avancé - MyCheckBox

---
+++ src/widgets/WgClass/MyQCheckBox.py
@@ -0,0 +1,40 @@
+from .MyQAbstractButton import MyQAbstractButton
+
+
+class MyQPushButton(MyQAbstractButton):
+    def __init__(
+            self,
+            widget,
+            size_policy_h,
+            size_policy_v,
+            fixed_width,
+            fixed_height,
+            minimum_width,
+            minimum_height,
+            maximum_width,
+            maximum_height,
+            font,
+            cursor,
+            focus_policy,
+            layout_direction,
+            txt,
+            ico,
+            ico_rgb,
+            ico_width,
+            ico_height,
+            checkable,
+            checked,
+            auto_repeat,
+            auto_exclusive,
+            auto_repeat_delay,
+            auto_repeat_interval,
+            auto_default,
+            default,
+            flat
+    ):
+        super().__init__(widget,size_policy_h,size_policy_v,fixed_width,fixed_height,minimum_width,minimum_height,maximum_width,maximum_height,font,cursor,focus_policy,layout_direction,txt,ico,ico_rgb,ico_width,ico_height,checkable,checked,auto_repeat,auto_exclusive,auto_repeat_delay,auto_repeat_interval)
+
+        # Default
+        widget.setAutoDefault(auto_default)
+        widget.setDefault(default)
+        widget.setFlat(flat)
