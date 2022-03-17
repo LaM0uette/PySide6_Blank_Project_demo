a0c1e132423c97f294fa27bbb62a687bb7597145
LaM0uette <LaM0uette.dev@gmail.com>
2022-03-17 20:15:26 +0100

    REMAKE - Refonte des WG avec héritage avancé - MyQCommandLinkButton

---
+++ src/widgets/MyCommandLinkButton.py
@@ -0,0 +1,29 @@
+from .WgClass.MyQCommandLinkButtonStyle import Style
+from src.lib.palettes import *
+
+
+##################
+##     BASE     ##
+##################
+class Base:
+    def __init__(self, widget):
+        self.widget = widget
+
+    def Base(self):
+        Style(
+            widget=self.widget,
+        )
+    def Transparent(self):
+        bg = Rgb().tr()
+
+        Style(
+            widget=self.widget,
+            bg=bg,
+            bg_hover=bg,
+            bg_checked=bg,
+            bg_checked_hover=bg,
+            bg_pressed=bg,
+            bg_checked_pressed=bg,
+            fg=Rgb().th3(),
+            fg_checked=Rgb().bn1(),
+        )
