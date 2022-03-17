dfb1bc3d81ea9bdf4a0d577d8601e2cd57e49524
LaM0uette <LaM0uette.dev@gmail.com>
2022-03-17 20:17:21 +0100

    REMAKE - Refonte des WG avec héritage avancé - MyQCommandLinkButton

--- src/widgets/WgClass/MyQCommandLinkButton.py
+++ src/widgets/WgClass/MyQCommandLinkButton.py
@@ -35,7 +35,7 @@ class MyQCommandLinkButton(MyQPushButton):
     ):
 
         # Default
-        super().__init__(widget, size_policy_h, size_policy_v, fixed_width, fixed_height, minimum_width, minimum_height, maximum_width, maximum_height, font, cursor, focus_policy, layout_direction, txt, ico, ico_rgb, ico_width, ico_height, checkable, checked, auto_repeat, auto_exclusive, auto_repeat_delay, auto_repeat_interval, auto_default, default, flat, description)
+        super().__init__(widget, size_policy_h, size_policy_v, fixed_width, fixed_height, minimum_width, minimum_height, maximum_width, maximum_height, font, cursor, focus_policy, layout_direction, txt, ico, ico_rgb, ico_width, ico_height, checkable, checked, auto_repeat, auto_exclusive, auto_repeat_delay, auto_repeat_interval, auto_default, default, flat)
 
         widget.setAutoDefault(auto_default)
         widget.setDefault(default)
