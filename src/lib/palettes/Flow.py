cfc1b9e08ee92933d978cbdf400a75409f334a28
LaM0uette <LaM0uette.dev@gmail.com>
2022-03-18 23:23:43 +0100

    Refont libs Flow

--- src/lib/palettes/Flow.py
+++ src/lib/palettes/Flow.py
@@ -3,5 +3,5 @@ from PySide6.QtWidgets import QListView
 
 class Flow:
 
-    def left_to_right(self): return QListView.LeftToRight
-    def top_to_bottom(self): return QListView.TopToBottom
+    LEFT_TO_RIGHT = QListView.LeftToRight
+    TOP_TO_BOTTOM = QListView.TopToBottom
