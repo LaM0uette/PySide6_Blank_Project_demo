f120d7b0f7a75c8ec4b6eb3048e2cb6bba3cf694
LaM0uette <LaM0uette.dev@gmail.com>
2022-03-18 23:51:16 +0100

    Refont libs TextFormat

--- src/lib/palettes/TextFormat.py
+++ src/lib/palettes/TextFormat.py
@@ -3,7 +3,7 @@ from PySide6.QtCore import Qt
 
 class TextFormat:
 
-    def plain(self): return Qt.PlainText
-    def rich(self): return Qt.RichText
-    def auto(self): return Qt.AutoText
-    def markdown(self): return Qt.MarkdownText
+    PLAIN = Qt.PlainText
+    RICH = Qt.RichText
+    AUTO = Qt.AutoText
+    MARKDOWN = Qt.MarkdownText
