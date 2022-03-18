59bdd862a3a69942ee598091a08e0c6d42216a79
LaM0uette <LaM0uette.dev@gmail.com>
2022-03-18 23:29:32 +0100

    Refont libs FrameShape

--- src/lib/palettes/FrameShape.py
+++ src/lib/palettes/FrameShape.py
@@ -3,10 +3,10 @@ from PySide6.QtWidgets import QFrame
 
 class FrameShape:
 
-    def no_frame(self): return QFrame.NoFrame
-    def box(self): return QFrame.Box
-    def panel(self): return QFrame.Panel
-    def win_panel(self): return QFrame.WinPanel
-    def h_line(self): return QFrame.HLine
-    def v_line(self): return QFrame.VLine
-    def styled_panel(self): return QFrame.StyledPanel
+    NO = QFrame.NoFrame
+    BOX = QFrame.Box
+    PANEL = QFrame.Panel
+    WIN_PANEL = QFrame.WinPanel
+    H_LINE = QFrame.HLine
+    V_LINE = QFrame.VLine
+    STYLED_PANEL = QFrame.StyledPanel
