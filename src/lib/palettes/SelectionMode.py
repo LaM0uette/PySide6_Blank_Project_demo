38931f4fb89e54a1e285b055bed2c7a18725b977
LaM0uette <LaM0uette.dev@gmail.com>
2022-03-18 23:45:36 +0100

    Refont libs SelectionMode

--- src/lib/palettes/SelectionMode.py
+++ src/lib/palettes/SelectionMode.py
@@ -3,8 +3,8 @@ from PySide6.QtWidgets import QAbstractItemView
 
 class SelectionMode:
 
-    def no(self): return QAbstractItemView.NoSelection
-    def single(self): return QAbstractItemView.SingleSelection
-    def multi(self): return QAbstractItemView.MultiSelection
-    def extended(self): return QAbstractItemView.ExtendedSelection
-    def contiguous(self): return QAbstractItemView.ContiguousSelection
+    NO = QAbstractItemView.NoSelection
+    SINGLE = QAbstractItemView.SingleSelection
+    MULTI = QAbstractItemView.MultiSelection
+    EXTENDED = QAbstractItemView.ExtendedSelection
+    CONTIGUOUS = QAbstractItemView.ContiguousSelection
