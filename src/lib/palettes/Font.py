51ecc59cb67704782e485ec8de66f60578d3283b
LaM0uette <LaM0uette.dev@gmail.com>
2022-03-19 00:22:27 +0100

    Refont libs Font

--- src/lib/palettes/Font.py
+++ src/lib/palettes/Font.py
@@ -3,25 +3,33 @@ from PySide6 import QtGui
 from src.config import config
 
 
-class Font:
-    H1 = 32             # A SUPPRIMER
-    H2 = 24             # A SUPPRIMER
-    H3 = 16             # A SUPPRIMER
-    H4 = 12             # A SUPPRIMER
-    H5 = 8              # A SUPPRIMER
+class FontSize:
+    H1 = 32
+    H2 = 24
+    H3 = 16
+    H4 = 12
+    H5 = 8
 
-    def base(self):
+
+class Font:
+
+    @staticmethod
+    def base():
         ft = QtGui.QFont()
         ft.setFamily(config.font)
-        ft.setPointSize(self.H4)
+        ft.setPointSize(FontSize.H4)
         return ft
-    def titre(self):
+
+    @staticmethod
+    def titre():
         ft = QtGui.QFont()
         ft.setFamily(config.font)
-        ft.setPointSize(self.H1)
+        ft.setPointSize(FontSize.H1)
         return ft
-    def texte(self):
+
+    @staticmethod
+    def texte():
         ft = QtGui.QFont()
         ft.setFamily(config.font)
-        ft.setPointSize(self.H5)
+        ft.setPointSize(FontSize.H5)
         return ft
