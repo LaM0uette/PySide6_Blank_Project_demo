c7184034d7bf0bc045ec896f959fa91ad21543cf
LaM0uette <LaM0uette.dev@gmail.com>
2022-03-18 22:48:22 +0100

    @dataclass libs AutoFormating

--- src/lib/palettes/AutoFormating.py
+++ src/lib/palettes/AutoFormating.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 
 @dataclass
 class AutoFormating:
+
     NONE: QTextEdit = QTextEdit.AutoNone
     BULLET_LIST: QTextEdit = QTextEdit.AutoBulletList
     ALL: QTextEdit = QTextEdit.AutoAll
