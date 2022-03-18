dcc636f28685d2a223216aa470293d3bb8763a94
LaM0uette <LaM0uette.dev@gmail.com>
2022-03-18 23:52:12 +0100

    Refont libs TickPosition

--- src/lib/palettes/TickPosition.py
+++ src/lib/palettes/TickPosition.py
@@ -3,9 +3,9 @@ from PySide6.QtWidgets import QSlider
 
 class TickPosition:
 
-    def no(self): return QSlider.NoTicks
-    def above(self): return QSlider.TicksAbove
-    def left(self): return QSlider.TicksLeft
-    def below(self): return QSlider.TicksBelow
-    def right(self): return QSlider.TicksRight
-    def both_sides(self): return QSlider.TicksBothSides
+    NO = QSlider.NoTicks
+    ABOVE = QSlider.TicksAbove
+    LEFT = QSlider.TicksLeft
+    BELOW = QSlider.TicksBelow
+    RIGHT = QSlider.TicksRight
+    BOTH_SIDES = QSlider.TicksBothSides
