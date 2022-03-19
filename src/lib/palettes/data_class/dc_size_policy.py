d5ea844a521cd5ecc3787e30a2559ee9c507b72d
LaM0uette <LaM0uette.dev@gmail.com>
2022-03-19 17:35:19 +0100

    REMAKE - Refonte des WG avec héritage avancé - Button - MyQWidget - ajout dataclass

--- src/lib/palettes/data_class/dc_size_policy.py
+++ src/lib/palettes/data_class/dc_size_policy.py
@@ -1,3 +1,9 @@
+"""
+dc_size_policy.%class(
+    horizontal: SizePolicy.%VALUE \n
+    vertical: SizePolicy.%VALUE)
+"""
+
 from dataclasses import dataclass
 
 from src.lib.palettes import SizePolicy
@@ -5,5 +11,6 @@ from src.lib.palettes import SizePolicy
 
 @dataclass
 class Base:
+
     horizontal: any = SizePolicy.PREFERED
     vertical: any = SizePolicy.PREFERED
