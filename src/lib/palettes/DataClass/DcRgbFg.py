a642bf86ff0e16cd75c314749b2cb6cce88f1e57
LaM0uette <lam0uette.dev@gmail.com>
2022-03-21 15:13:08 +0100

    REMAKE - Refonte des WG avec héritage avancé - Button - MyQPushButtonStyle - ajout dataclass

--- src/lib/palettes/DataClass/DcRgbFg.py
+++ src/lib/palettes/DataClass/DcRgbFg.py
@@ -1,9 +1,19 @@
 """
-DcAutoActions.%class(
-    auto_repeat: bool = % \n
-    auto_exclusive: bool = % \n
-    auto_repeat_delay: int = % \n
-    auto_repeat_interval: int = %)
+DcRgbFg.%class(
+    gen = PaRgb.%
+
+    base: tuple = PaRgb.%
+    hover: tuple = PaRgb.%
+    indeterminate: tuple = PaRgb.%
+    indeterminate_hover: tuple = PaRgb.%
+    checked: tuple = PaRgb.%
+    checked_hover: tuple = PaRgb.%
+
+    pressed: tuple = PaRgb.%
+    indeterminate_pressed: tuple = PaRgb.%
+    checked_pressed: tuple = PaRgb.%
+
+    selection: tuple = PaRgb.%)
 """
 from dataclasses import dataclass
 
@@ -12,17 +22,31 @@ from src.lib.palettes import *
 
 @dataclass
 class Base:
+    gen: tuple = None
 
-    bg = PaRgb.TH3
-    bg_hover = PaRgb.TH3
-    bg_checked = PaRgb.TH1
-    bg_checked_hover = PaRgb.TH1
-    bg_pressed = PaRgb.TH3
-    bg_checked_pressed = PaRgb.TH1
+    base: tuple = PaRgb.TH1
+    hover: tuple = PaRgb.BN1
+    indeterminate: tuple = PaRgb.TH1
+    indeterminate_hover: tuple = PaRgb.TH1
+    checked: tuple = PaRgb.TH3
+    checked_hover: tuple = PaRgb.BN1
 
-    fg = PaRgb.TH1
-    fg_hover = PaRgb.BN1
-    fg_checked = PaRgb.TH3
-    fg_checked_hover = PaRgb.BN1
-    fg_pressed = PaRgb.BN2
-    fg_checked_pressed = PaRgb.BN2
+    pressed: tuple = PaRgb.BN2
+    indeterminate_pressed: tuple = PaRgb.BN2
+    checked_pressed: tuple = PaRgb.BN2
+
+    selection: tuple = PaRgb.TH3
+
+
+    def __post_init__(self):
+        if self.gen:
+            self.base = self.gen
+            self.hover = self.gen
+            self.indeterminate = self.gen
+            self.indeterminate_hover = self.gen
+            self.checked = self.gen
+            self.checked_hover = self.gen
+
+            self.pressed = self.gen
+            self.indeterminate_pressed = self.gen
+            self.checked_pressed = self.gen
