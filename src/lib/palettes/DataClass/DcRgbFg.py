957f79275458e9da3cfeb93280d8b34de4d865e9
LaM0uette <lam0uette.dev@gmail.com>
2022-03-21 14:20:54 +0100

    REMAKE - Refonte des WG avec héritage avancé - Button - MyAbstractButton - ajout dataclass

---
+++ src/lib/palettes/DataClass/DcRgbFg.py
@@ -0,0 +1,28 @@
+"""
+DcAutoActions.%class(
+    auto_repeat: bool = % \n
+    auto_exclusive: bool = % \n
+    auto_repeat_delay: int = % \n
+    auto_repeat_interval: int = %
+"""
+from dataclasses import dataclass
+
+from src.lib.palettes import *
+
+
+@dataclass
+class Base:
+
+    bg = PaRgb.TH3
+    bg_hover = PaRgb.TH3
+    bg_checked = PaRgb.TH1
+    bg_checked_hover = PaRgb.TH1
+    bg_pressed = PaRgb.TH3
+    bg_checked_pressed = PaRgb.TH1
+
+    fg = PaRgb.TH1
+    fg_hover = PaRgb.BN1
+    fg_checked = PaRgb.TH3
+    fg_checked_hover = PaRgb.BN1
+    fg_pressed = PaRgb.BN2
+    fg_checked_pressed = PaRgb.BN2
--- src/lib/palettes/DataClass/__init__.py
+++ src/lib/palettes/DataClass/__init__.py
@@ -1,4 +1,6 @@
 from . import DcAutoActions
 from . import DcDim
 from . import DcIco
+from . import DcRgbBg
+from . import DcRgbFg
 from . import DcSizePolicy
