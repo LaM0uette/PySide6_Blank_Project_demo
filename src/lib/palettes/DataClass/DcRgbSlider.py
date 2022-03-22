125b215fd1c35720cf4bd93309e1ead7805c0019
LaM0uette <lam0uette.dev@gmail.com>
2022-03-22 16:48:52 +0100

    REMAKE - Refonte des WG avec héritage avancé - Button - MyQPushButtonStyle - ajout dataclass

---
+++ src/lib/palettes/DataClass/DcRgbSlider.py
@@ -0,0 +1,40 @@
+"""
+DcRgbFg.%class(
+    groove_gen: tuple = %
+    handle_gen: tuple = %
+
+    groove: tuple = PaRgb.%
+    groove_hover: tuple = PaRgb.%
+    groove_pressed: tuple = PaRgb.%
+    handle: tuple = PaRgb.%
+    handle_hover: tuple = PaRgb.%
+    handle_pressed: tuple = PaRgb.%
+"""
+from dataclasses import dataclass
+
+from src.lib.palettes import *
+
+
+@dataclass
+class Base:
+    groove_gen: tuple = None
+    handle_gen: tuple = None
+
+    groove: tuple = PaRgb.TH3
+    groove_hover: tuple = PaRgb.TH3
+    groove_pressed: tuple = PaRgb.TH3
+    handle: tuple = PaRgb.TH2
+    handle_hover: tuple = PaRgb.TH2
+    handle_pressed: tuple = PaRgb.BN1
+
+
+    def __post_init__(self):
+        if self.groove_gen:
+            self.groove = self.groove_gen
+            self.groove_hover = self.groove_gen
+            self.groove_pressed = self.groove_gen
+
+        if self.handle_gen:
+            self.handle = self.handle_gen
+            self.handle_hover = self.handle_gen
+            self.handle_pressed = self.handle_gen
