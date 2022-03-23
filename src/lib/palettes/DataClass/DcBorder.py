c37db06adbf0fd0e47b2f9085a1b3db86792ea57
LaM0uette <lam0uette.dev@gmail.com>
2022-03-23 11:18:59 +0100

    REMAKE - Refonte des WG avec héritage avancé - Button - MyQPushButtonStyle - ajout dataclass

--- src/lib/palettes/DataClass/DcBorder.py
+++ src/lib/palettes/DataClass/DcBorder.py
@@ -1,9 +1,23 @@
 """
-DcIco.%class(
-    ico: str = PaImg.% \n
-    ico_rgb: str = "%" \n
-    ico_width: int = PaDim.% \n
-    ico_height: int = PaDim.%)
+DcBorder.%class(
+    gen: tuple = %
+    gen_style: tuple = %
+    gen_rgb: tuple = %
+
+    base: tuple = (%, %, %, %)
+    base_style: str = "%"
+    base_rgb: str = PaRgb.%
+    hover: tuple = (%, %, %, %)
+    hover_style: str = "%"
+    hover_rgb: str = PaRgb.%
+    checked: tuple = (%, %, %, %)
+    checked_style: str = "%"
+    checked_rgb: str = PaRgb.%
+    checked_hover: tuple = (%, %, %, %)
+    checked_hover_style: str = "%"
+    checked_hover_rgb: str = PaRgb.%
+
+    radius: tuple = (%, %, %, %))
 """
 
 from dataclasses import dataclass
@@ -13,6 +27,9 @@ from src.lib.palettes import PaRgb
 
 @dataclass
 class Base:
+    gen: tuple = None
+    gen_style: tuple = None
+    gen_rgb: tuple = None
 
     base: tuple = (0,)*4
     base_style: str = "solid"
@@ -28,3 +45,19 @@ class Base:
     checked_hover_rgb: str = None
 
     radius: tuple = (3,)*4
+
+    def __post_init__(self):
+        if self.gen:
+            self.hover = self.gen
+            self.checked = self.gen
+            self.checked_hover = self.gen
+
+        if self.gen_style:
+            self.hover_style = self.gen_style
+            self.checked_style = self.gen_style
+            self.checked_hover_style = self.gen_style
+
+        if self.gen_rgb:
+            self.hover_rgb = self.gen_rgb
+            self.checked_rgb = self.gen_rgb
+            self.checked_hover_rgb = self.gen_rgb
