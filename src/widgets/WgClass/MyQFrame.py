65b6bcc3f8ce587b8c833980f90160a891098e5d
LaM0uette <lam0uette.dev@gmail.com>
2022-03-23 13:44:55 +0100

    REMAKE - Refonte des WG avec héritage avancé - Button - MyQPushButtonStyle - ajout dataclass

---
+++ src/widgets/WgClass/MyQFrame.py
@@ -0,0 +1,26 @@
+from .MyQWidget import MyQWidget
+
+
+class MyQAbstractButton(MyQWidget):
+    def __init__(
+            self,
+            widget,
+            size_policy,
+            dim,
+            font,
+            cursor,
+            focus_policy,
+            layout_direction,
+
+            frame_shape,
+            frame_shadow,
+            line_width,
+
+            shadow,
+    ):
+        super().__init__(widget,size_policy,dim,font,cursor,focus_policy,layout_direction)
+
+        widget.setFrameShape(frame_shape)
+        widget.setFrameShadow(frame_shadow)
+        widget.setLineWidth(line_width)
+        if shadow: widget.setGraphicsEffect(shadow)
