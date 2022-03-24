b6363bb39fe47bc9fbd5fc979f06504f94950ad9
LaM0uette <lam0uette.dev@gmail.com>
2022-03-24 09:19:18 +0100

    REMAKE - Refonte des WG avec héritage avancé - Button - MyQScrollArea

---
+++ src/widgets/WgClass/MyQScrollArea.py
@@ -0,0 +1,20 @@
+from .MyQAbstractScrollArea import MyQAbstractScrollArea
+
+
+class MyQScrollArea(MyQAbstractScrollArea):
+    def __init__(
+            self,
+            widget,
+            size_policy,
+            dim,
+            font,
+            cursor,
+            focus_policy,
+            layout_direction,
+
+            frame,
+
+            scroll_policy,
+    ):
+        super().__init__(widget, size_policy, dim, font, cursor, focus_policy, layout_direction, frame, scroll_policy)
+
