6fd6ebecaefdc1cfc1994114875cf9c7cbdf8cf5
LaM0uette <lam0uette.dev@gmail.com>
2022-03-24 09:26:23 +0100

    REMAKE - Refonte des WG avec héritage avancé - Button - MyQScrollArea

--- src/widgets/WgClass/MyQScrollAreaStyle.py
+++ src/widgets/WgClass/MyQScrollAreaStyle.py
@@ -1,25 +1,26 @@
 from .MyQScrollArea import MyQScrollArea
+from src.lib.globals import v_wg
 
 
 class Style(MyQScrollArea):
     def __init__(
             self,
             widget,
-            size_policy,
-            dim,
-            font,
-            cursor,
-            focus_policy,
-            layout_direction,
+            size_policy=v_wg.SIZE_POLICY,
+            dim=v_wg.DIM_WG,
+            font=v_wg.FONT,
+            cursor=v_wg.CUR_ACTION,
+            focus_policy=v_wg.FOCUS_POLICY,
+            layout_direction=v_wg.LAYOUT_DIRECTION,
 
-            frame,
+            frame=v_wg.FRAME,
 
-            scroll_policy,
+            scroll_policy=v_wg.SCROLL_POLICY,
 
             resizable=True,
 
 
     ):
-        super().__init__(widget, size_policy, dim, font, cursor, focus_policy, layout_direction, frame, scroll_policy)
+        super().__init__(widget, size_policy, dim, font, cursor, focus_policy, layout_direction, frame, scroll_policy, resizable)
 
-        widget.setWidgetResizable(resizable)
+
