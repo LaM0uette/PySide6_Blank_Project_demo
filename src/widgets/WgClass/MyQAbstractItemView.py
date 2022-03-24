9ddf574be57e6633514b94c35170bc1a2bbac065
LaM0uette <lam0uette.dev@gmail.com>
2022-03-24 15:58:00 +0100

    REMAKE - Refonte des WG avec héritage avancé - Button - MyQAbstractItemViewStyle

--- src/widgets/WgClass/MyQAbstractItemView.py
+++ src/widgets/WgClass/MyQAbstractItemView.py
@@ -19,9 +19,7 @@ class MyQAbstractItemView(MyQAbstractScrollArea):
             auto_scroll,
             auto_scroll_margin,
             tab_navigation,
-            drag_enabled,
-            drag_drop_mode,
-            drop_action,
+            drag_drop,
             alternative_row_colors,
             selection_mode,
             selection_behavior,
@@ -31,9 +29,9 @@ class MyQAbstractItemView(MyQAbstractScrollArea):
         widget.setAutoScroll(auto_scroll)
         widget.setAutoScrollMargin(auto_scroll_margin)
         widget.setTabKeyNavigation(tab_navigation)
-        widget.setDragEnabled(drag_enabled)
-        widget.setDragDropMode(drag_drop_mode)
-        widget.setDefaultDropAction(drop_action)
+        widget.setDragEnabled(drag_drop.drag_enabled)
+        widget.setDragDropMode(drag_drop.drag_drop_mode)
+        widget.setDefaultDropAction(drag_drop.drop_action)
         widget.setAlternatingRowColors(alternative_row_colors)
         widget.setSelectionMode(selection_mode)
         widget.setSelectionBehavior(selection_behavior)
