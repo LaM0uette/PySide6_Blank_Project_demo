b74b7c13cd5dba46c1839a358ab3b665915770e1
LaM0uette <LaM0uette.dev@gmail.com>
2022-03-26 13:39:02 +0100

    REMAKE - Refonte des WG avec héritage avancé - Button - MyQlistView

--- src/widgets/WgClass/MyQListView.py
+++ src/widgets/WgClass/MyQListView.py
@@ -23,6 +23,13 @@ class MyQListView(MyQAbstractItemView):
             alternative_row_colors,
             selection_mode,
             selection_behavior,
+
+            movement,
+            flow,
+            items_spacing,
     ):
         super().__init__(widget, size_policy, dim, font, cursor, focus_policy, layout_direction, frame, scroll_policy, auto_scroll, auto_scroll_margin, tab_navigation, drag_drop, alternative_row_colors, selection_mode, selection_behavior)
 
+        widget.setMovement(movement)
+        widget.setFlow(flow)
+        widget.setSpacing(items_spacing)
