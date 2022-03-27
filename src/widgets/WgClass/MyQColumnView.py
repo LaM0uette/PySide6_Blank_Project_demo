cf6f22d3bedbdfeb9105c4fd3c79207134e29571
LaM0uette <LaM0uette.dev@gmail.com>
2022-03-27 14:27:27 +0200

    REMAKE - Refonte des WG avec héritage avancé - Button - Création des classes

--- src/widgets/WgClass/MyQColumnView.py
+++ src/widgets/WgClass/MyQColumnView.py
@@ -16,16 +16,13 @@ class MyQColumnView(MyQAbstractItemView):
 
             scroll_policy,
 
-            auto_scroll,
-            auto_scroll_margin,
-            tab_navigation,
+            abstract_item_view,
             drag_drop,
-            alternative_row_colors,
             selection_mode,
             selection_behavior,
 
             resize_grips_visible,
     ):
-        super().__init__(widget, size_policy, dim, font, cursor, focus_policy, layout_direction, frame, scroll_policy, auto_scroll, auto_scroll_margin, tab_navigation, drag_drop, alternative_row_colors, selection_mode, selection_behavior)
+        super().__init__(widget, size_policy, dim, font, cursor, focus_policy, layout_direction, frame, scroll_policy, abstract_item_view,drag_drop,selection_mode,selection_behavior)
 
         widget.setResizeGripsVisible(resize_grips_visible)
