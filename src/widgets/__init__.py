836b6fe9f5a08c0afadede9c99c5e07f191d3b5e
LaM0uette <lam0uette.dev@gmail.com>
2022-03-28 13:26:44 +0200

    REMAKE - Refonte des WG avec héritage avancé - Button - Création des classes

--- src/widgets/__init__.py
+++ src/widgets/__init__.py
@@ -35,3 +35,4 @@ from src.widgets import MyWidget
 from src.widgets import MyDockWidget
 from src.widgets import MyComboBox
 from src.widgets import MyFontComboBox
+from src.widgets import MyLineEdit
