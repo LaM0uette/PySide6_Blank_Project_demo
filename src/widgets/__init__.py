2a76c55c0f011e4fbd8f0df1b2a63aadb2e46a5e
LaM0uette <lam0uette.dev@gmail.com>
2022-03-28 16:45:24 +0200

    REMAKE - Refonte des WG avec héritage avancé - ajout des wg dans le main

--- src/widgets/__init__.py
+++ src/widgets/__init__.py
@@ -1,14 +1,9 @@
 from src.widgets.ComboBox import ComboBox
-from src.widgets.DateEdit import DateEdit
 from src.widgets.Label import Label
 from src.widgets.LineEdit import LineEdit
-from src.widgets.PlainTextEdit import PlainTextEdit
-from src.widgets.ProgressBar import ProgressBar
 from src.widgets.Slider import Slider
 from src.widgets.SpinBox import SpinBox
-from src.widgets.TableWidget import TableWidget
 from src.widgets.TextEdit import TextEdit
-from src.widgets.ToolBox import ToolBox
 from src.widgets.TrayIcon import TrayIcon
 from src.widgets.TreeWidget import TreeWidget
 
