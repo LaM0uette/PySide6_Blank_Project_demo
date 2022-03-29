8b9a3f0e3e15c1d0c96afb880447c606a13b8eab
LaM0uette <LaM0uette.dev@gmail.com>
2022-03-29 19:02:16 +0200

    REMAKE - Refonte des WG avec héritage avancé - ajout des wg dans le main

--- src/widgets/WgClass/MyQLabelStyle.py
+++ src/widgets/WgClass/MyQLabelStyle.py
@@ -24,5 +24,53 @@ class Style(MyQLabel):
             word_wrap=False,
             indent=0,
             open_external_link=False,
+
+            background=v_wg.BACKGROUND,
+            foreground=v_wg.FOREGROUND,
+            margin=(0,) * 4,
+            padding=(0,) * 4,
+            border=v_wg.BORDER,
     ):
         super().__init__(widget, size_policy, dim, font, cursor, focus_policy, layout_direction, frame,text_format,pixmap,pixmap_rgb,scaled_contents,align,word_wrap,indent,open_external_link)
+
+        style = f"""
+                /* LABEL */
+                .QLabel {{
+                background-color: rgba{background.base};
+                color: rgba{foreground.base};
+                margin-top: {margin[0]}px;
+                margin-bottom: {margin[1]}px;
+                margin-right: {margin[2]}px;
+                margin-left: {margin[3]}px;
+                padding-top: {padding[0]}px;
+                padding-bottom: {padding[1]}px;
+                padding-right: {padding[2]}px;
+                padding-left: {padding[3]}px;
+                }}
+                .QLabel:hover {{
+                background-color: rgba{background.hover};
+                color: rgba{foreground.hover};
+                }}
+
+                /* BORDURES */
+                .QLabel {{
+                border-top: {border.base[0]}px {border.base_style} rgba{border.base_rgb};
+                border-bottom: {border.base[1]}px {border.base_style} rgba{border.base_rgb};
+                border-right: {border.base[2]}px {border.base_style} rgba{border.base_rgb};
+                border-left: {border.base[3]}px {border.base_style} rgba{border.base_rgb};
+                }}
+                .QLabel:hover {{
+                border-top: {border.hover[0]}px {border.hover_style} rgba{border.hover_rgb};
+                border-bottom: {border.hover[1]}px {border.hover_style} rgba{border.hover_rgb}
+                border-right: {border.hover[2]}px {border.hover_style} rgba{border.hover_rgb};
+                border-left: {border.hover[3]}px {border.hover_style} rgba{border.hover_rgb};
+                }}
+
+                /* RAYONS */
+                .QLabel {{
+                border-top-right-radius: {border.radius[0]}px;
+                border-top-left-radius: {border.radius[1]}px;
+                border-bottom-right-radius: {border.radius[2]}px;
+                border-bottom-left-radius: {border.radius[3]}px;
+                }}"""
+        widget.setStyleSheet(style)
